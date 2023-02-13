b33ccc8d3eaa1a700116edf9e6c71d3993d5f2dc
Alexander Valchev <106471266+AlexanderRVulchev@users.noreply.github.com>
2023-02-13 18:57:45 +0200

    Add files via upload

---
+++ 01. Entity Framework Introduction/05. Employees from Research and Development/StartUp.cs
@@ -0,0 +1,37 @@
+﻿using SoftUni.Data;
+using System;
+using System.Linq;
+using System.Text;
+
+namespace SoftUni
+{
+    public class StartUp
+    {
+        static void Main()
+        {
+            var context = new SoftUniContext();
+            string output = GetEmployeesFromResearchAndDevelopment(context);
+            Console.WriteLine(output);
+        }
+
+        public static string GetEmployeesFromResearchAndDevelopment(SoftUniContext context)
+        {
+            var employeesInfo = context.Employees
+                .Where(x => x.Department.Name == "Research and Development")
+                .Select(x => new {x.FirstName, x.LastName, x.Department, x.Salary})
+                .OrderBy(x => x.Salary)
+                .ThenByDescending(x => x.FirstName)
+                .ToList();
+
+            StringBuilder sb = new StringBuilder();
+
+            foreach (var e in employeesInfo)
+            {
+                sb.AppendLine($"{e.FirstName} {e.LastName} from {e.Department.Name} - ${e.Salary:f2}");
+            }
+
+            return sb.ToString().TrimEnd();
+        }
+    }
+
+}
