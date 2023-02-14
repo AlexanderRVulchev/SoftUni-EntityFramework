914e91639a6548af1c09b6d4e60b206f8e5822cb
Alexander Valchev <106471266+AlexanderRVulchev@users.noreply.github.com>
2023-02-14 18:15:17 +0200

    Add files via upload

---
+++ 01. Entity Framework Introduction/08. Addresses by Town/StartUp.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+
+namespace SoftUni
+{
+    using Data;
+    using Models;
+    
+    public class StartUp
+    {
+        static void Main()
+        {
+            var context = new SoftUniContext();
+            string output = GetAddressesByTown(context);
+            Console.WriteLine(output);
+        }
+
+        public static string GetAddressesByTown(SoftUniContext context)
+        {
+            string[] addressesInfo = context.Addresses
+                .OrderByDescending(a => a.Employees.Count)
+                .ThenBy(a => a.Town.Name)
+                .ThenBy(a => a.AddressText)
+                .Take(10)
+                .Select(a => $"{a.AddressText}, {a.Town.Name} - {a.Employees.Count} employees")
+                .ToArray();
+
+            return string.Join(Environment.NewLine, addressesInfo);
+        }
+    }
+}
