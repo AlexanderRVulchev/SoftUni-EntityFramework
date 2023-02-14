90312583d713c0b346fbffd43898417a855b4b04
Alexander Valchev <106471266+AlexanderRVulchev@users.noreply.github.com>
2023-02-14 18:17:21 +0200

    Add files via upload

---
+++ 01. Entity Framework Introduction/11. Find Latest 10 Projects/StartUp.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+using System.Globalization;
+
+namespace SoftUni
+{
+    using Data;
+
+    public class StartUp
+    {
+        static void Main()
+        {
+            var context = new SoftUniContext();
+            string output = GetLatestProjects(context);
+            Console.WriteLine(output);
+        }
+
+        public static string GetLatestProjects(SoftUniContext context)
+        {
+            var projectsInfo = context.Projects
+                .OrderByDescending(p => p.StartDate)
+                .Take(10)
+                .OrderBy(p => p.Name)
+                .Select(p => new
+                {
+                    p.Name,
+                    p.Description,
+                    StartDate = p.StartDate.ToString("M/d/yyyy h:mm:ss tt", CultureInfo.InvariantCulture),
+                })
+                .ToArray();
+
+            StringBuilder sb = new StringBuilder();
+            foreach (var p in projectsInfo)
+            {
+                sb.AppendLine(p.Name);
+                sb.AppendLine(p.Description);
+                sb.AppendLine(p.StartDate);
+            }
+
+            return sb.ToString().TrimEnd();
+        }
+    }
+}
