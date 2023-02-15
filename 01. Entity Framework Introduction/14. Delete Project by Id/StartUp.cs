8638f164a84c0bbd8e1305bc349c2e727cba42a9
Alexander Valchev <106471266+AlexanderRVulchev@users.noreply.github.com>
2023-02-15 19:55:39 +0200

    Add files via upload

---
+++ 01. Entity Framework Introduction/14. Delete Project by Id/StartUp.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+
+
+namespace SoftUni
+{
+    using Data;
+    using SoftUni.Models;
+
+    public class StartUp
+    {
+        static void Main()
+        {
+            var context = new SoftUniContext();
+            string output = DeleteProjectById(context);
+            Console.WriteLine(output);
+        }
+
+        public static string DeleteProjectById(SoftUniContext context)
+        {
+            var employeesProjectsToDelete = context.EmployeesProjects.Where(ep => ep.ProjectId == 2);
+            context.EmployeesProjects.RemoveRange(employeesProjectsToDelete);
+
+            var projectToDelete = context.Projects.Where(p => p.ProjectId == 2);
+            context.Projects.RemoveRange(projectToDelete);
+
+            context.SaveChanges();
+
+            string[] projectsNames = context.Projects
+                .Take(10)
+                .Select(p => p.Name)
+                .ToArray();
+
+            return string.Join(Environment.NewLine, projectsNames);
+        }
+    }
+}
