e91b17e186a4fed189a0bbb7c7a4a94515ee808a
Alexander Valchev <106471266+AlexanderRVulchev@users.noreply.github.com>
2023-02-15 19:55:59 +0200

    Add files via upload

---
+++ 01. Entity Framework Introduction/15. Remove Town/StartUp.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Linq;
+
+
+namespace SoftUni
+{
+    using Data;
+    using Models;
+    
+    public class StartUp
+    {
+        static void Main()
+        {
+            var context = new SoftUniContext();
+            string output = RemoveTown(context);
+            Console.WriteLine(output);
+        }
+
+        public static string RemoveTown(SoftUniContext context)
+        {
+            Town townToDelete = context.Towns
+                .Where(t => t.Name == "Seattle")
+                .FirstOrDefault();
+
+            Address[] addressesToDelete = context.Addresses
+                .Where(a => a.TownId == townToDelete.TownId)
+                .ToArray();
+
+            Employee[] employeesToRemoveAddressFrom = context.Employees
+                .Where(e => addressesToDelete
+                .Contains(e.Address))
+                .ToArray();
+
+            foreach (Employee e in employeesToRemoveAddressFrom)
+            {
+                e.AddressId = null;
+            }
+
+            context.Addresses.RemoveRange(addressesToDelete);
+            context.Towns.Remove(townToDelete);
+            context.SaveChanges();
+
+            return $"{addressesToDelete.Count()} addresses in Seattle were deleted";
+        }
+    }
+}
