a7be1767fbd159fd303f3b2a43b4b24b9ec9beae
Alexander Valchev <106471266+AlexanderRVulchev@users.noreply.github.com>
2023-02-16 18:33:55 +0200

    Add files via upload

---
+++ 02. Entity Relations/01. Student System (with Fluent API)/Data/Models/Course.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace P01_StudentSystem.Data.Models
+{    
+    public class Course
+    {
+        public Course()
+        {
+            this.Resources = new HashSet<Resource>();
+            this.StudentsEnrolled = new HashSet<StudentCourse>();
+            this.HomeworkSubmissions = new HashSet<Homework>();
+        }
+                
+        public int CourseId { get; set; }
+                
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+                
+        public DateTime StartDate { get; set; }
+                
+        public DateTime EndDate { get; set; }
+                
+        public decimal Price { get; set; }
+
+        public ICollection<Resource> Resources { get; set; }
+
+        public ICollection<StudentCourse> StudentsEnrolled { get; set; }
+
+        public ICollection<Homework> HomeworkSubmissions { get; set; }
+    }
+
+   
+}
