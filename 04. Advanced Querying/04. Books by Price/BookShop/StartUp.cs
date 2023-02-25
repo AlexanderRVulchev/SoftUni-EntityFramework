2163112134a5f6c569cf20da792872ad36f31d4b
Alexander Valchev <106471266+AlexanderRVulchev@users.noreply.github.com>
2023-02-25 15:58:06 +0200

    Add files via upload

---
+++ 04. Advanced Querying/04. Books by Price/BookShop/StartUp.cs
@@ -0,0 +1,30 @@
+﻿namespace BookShop
+{
+    using Data;
+    using Initializer;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            using var db = new BookShopContext();
+            //DbInitializer.ResetDatabase(db);
+
+            Console.WriteLine(GetBooksByPrice(db));
+        }
+
+        public static string GetBooksByPrice(BookShopContext context)
+        {
+            string[] booksByPrice = context
+                .Books
+                .Where(b => b.Price > 40)
+                .OrderByDescending(b => b.Price)
+                .Select(b => $"{b.Title} - ${b.Price:f2}")
+                .ToArray();
+
+            return string.Join(Environment.NewLine, booksByPrice);
+        }
+    }
+}
+
+
