ad55ca08e58402be586c22304c0588dee0480db2
Alexander Valchev <106471266+AlexanderRVulchev@users.noreply.github.com>
2023-02-25 15:57:07 +0200

    Add files via upload

---
+++ 04. Advanced Querying/03. Golden Books/BookShop/StartUp.cs
@@ -0,0 +1,32 @@
+﻿namespace BookShop
+{
+    using BookShop.Models.Enums;
+    using Data;
+    using Initializer;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            using var db = new BookShopContext();
+            //DbInitializer.ResetDatabase(db);
+
+            Console.WriteLine(GetGoldenBooks(db));
+        }
+
+        public static string GetGoldenBooks(BookShopContext context)
+        {
+            string[] goldenBooksTitles = context
+                .Books
+                .Where(b => b.EditionType == EditionType.Gold &&
+                            b.Copies < 5000)
+                .OrderBy(b => b.BookId)
+                .Select(b => b.Title)
+                .ToArray();
+
+            return String.Join(Environment.NewLine, goldenBooksTitles);
+        }
+    }
+}
+
+
