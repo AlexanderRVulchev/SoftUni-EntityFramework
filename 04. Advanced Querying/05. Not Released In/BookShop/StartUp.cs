3d7de6ccaa5fd7464532c4504cde49f1036a1062
Alexander Valchev <106471266+AlexanderRVulchev@users.noreply.github.com>
2023-02-25 15:59:13 +0200

    Add files via upload

---
+++ 04. Advanced Querying/05. Not Released In/BookShop/StartUp.cs
@@ -0,0 +1,31 @@
+﻿namespace BookShop
+{
+    using Data;
+    using Initializer;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            using var db = new BookShopContext();
+            //DbInitializer.ResetDatabase(db);
+
+            int year = int.Parse(Console.ReadLine());
+            Console.WriteLine(GetBooksNotReleasedIn(db, year));
+        }
+
+        public static string GetBooksNotReleasedIn(BookShopContext context, int year)
+        {
+            string[] booksNotReleasedIn = context
+                .Books
+                .Where(b => b.ReleaseDate.Value.Year != year)
+                .OrderBy(b => b.BookId)
+                .Select(b => b.Title)
+                .ToArray();
+
+            return string.Join(Environment.NewLine, booksNotReleasedIn);
+        }
+    }
+}
+
+
