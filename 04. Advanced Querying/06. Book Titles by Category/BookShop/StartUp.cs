f5432a215d222bdc79c7e008676330a66a30c388
Alexander Valchev <106471266+AlexanderRVulchev@users.noreply.github.com>
2023-02-25 16:00:10 +0200

    Add files via upload

---
+++ 04. Advanced Querying/06. Book Titles by Category/BookShop/StartUp.cs
@@ -0,0 +1,34 @@
+﻿namespace BookShop
+{
+    using Data;
+    using Initializer;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            using var db = new BookShopContext();
+            //DbInitializer.ResetDatabase(db);
+
+            string input = Console.ReadLine();
+            Console.WriteLine(GetBooksByCategory(db, input));
+        }
+
+        public static string GetBooksByCategory(BookShopContext context, string input)
+        {
+            string[] categories = input.ToLower().Split().ToArray();
+            
+
+            string[] bookTitlesByCategories = context
+                .BooksCategories
+                .Where(bc => categories.Contains(bc.Category.Name.ToLower()))
+                .Select(bc => bc.Book.Title)
+                .OrderBy(t => t)
+                .ToArray();
+
+            return String.Join(Environment.NewLine, bookTitlesByCategories);
+        }
+    }
+}
+
+
