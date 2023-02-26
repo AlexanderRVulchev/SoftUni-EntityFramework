36841541bedf0acda337e5c74c68dc0297f20bd9
Alexander Valchev <106471266+AlexanderRVulchev@users.noreply.github.com>
2023-02-26 11:10:55 +0200

    Add files via upload

---
+++ 04. Advanced Querying/07. Released Before Date/BookShop/StartUp.cs
@@ -0,0 +1,33 @@
+﻿namespace BookShop
+{
+    using Data;
+    using Initializer;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            using var db = new BookShopContext();
+            //DbInitializer.ResetDatabase(db);
+
+            string input = Console.ReadLine();
+            Console.WriteLine(GetBooksReleasedBefore(db, input));
+        }
+
+        public static string GetBooksReleasedBefore(BookShopContext context, string date)
+        {
+            DateTime parsedDate = DateTime.ParseExact(date, "dd-MM-yyyy", null);
+
+            string[] booksReleasedBefore = context
+                .Books
+                .Where(b => b.ReleaseDate < parsedDate)
+                .OrderByDescending(b => b.ReleaseDate)
+                .Select(b => $"{b.Title} - {b.EditionType.ToString()} - ${b.Price:f2}")
+                .ToArray();
+
+            return string.Join(Environment.NewLine, booksReleasedBefore);
+        }
+    }
+}
+
+
