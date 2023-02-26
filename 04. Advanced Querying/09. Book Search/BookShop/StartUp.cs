57430982d68fa6dca0c75d5d285d8c069d11dc5a
Alexander Valchev <106471266+AlexanderRVulchev@users.noreply.github.com>
2023-02-26 11:12:58 +0200

    Add files via upload

---
+++ 04. Advanced Querying/09. Book Search/BookShop/StartUp.cs
@@ -0,0 +1,32 @@
+﻿namespace BookShop
+{
+    using Data;
+    using Initializer;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            using var db = new BookShopContext();
+            //DbInitializer.ResetDatabase(db);
+
+            string input = Console.ReadLine();
+            Console.WriteLine(GetBookTitlesContaining(db, input));
+
+        }
+
+        public static string GetBookTitlesContaining(BookShopContext context, string input)
+        {
+            string[] bookTitlesContaining = context
+                .Books
+                .Where(b => b.Title.ToLower().Contains(input.ToLower()))
+                .Select(b => b.Title)
+                .OrderBy(title => title)
+                .ToArray();
+
+            return String.Join(Environment.NewLine, bookTitlesContaining);
+        }
+    }
+}
+
+
