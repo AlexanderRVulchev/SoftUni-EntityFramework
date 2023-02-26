02f307c7e229d2f23d7da12d5aeef0693d99f133
Alexander Valchev <106471266+AlexanderRVulchev@users.noreply.github.com>
2023-02-26 11:11:55 +0200

    Add files via upload

---
+++ 04. Advanced Querying/08. Author Search/BookShop/StartUp.cs
@@ -0,0 +1,31 @@
+﻿namespace BookShop
+{
+    using Data;
+    using Initializer;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            using var db = new BookShopContext();
+            //DbInitializer.ResetDatabase(db);
+
+            string input = Console.ReadLine();
+            Console.WriteLine(GetAuthorNamesEndingIn(db, input));
+        }
+
+        public static string GetAuthorNamesEndingIn(BookShopContext context, string input)
+        {
+            string[] authorNamesEndingIn = context
+                .Authors
+                .Where(a => a.FirstName.EndsWith(input))
+                .Select(a => a.FirstName + " " + a.LastName)
+                .OrderBy(name => name)
+                .ToArray();
+
+            return string.Join(Environment.NewLine, authorNamesEndingIn);
+        }
+    }
+}
+
+
