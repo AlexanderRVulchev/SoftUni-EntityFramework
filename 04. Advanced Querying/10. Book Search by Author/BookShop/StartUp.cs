d7865c3588b239e7be802c864213de0dd41197a2
Alexander Valchev <106471266+AlexanderRVulchev@users.noreply.github.com>
2023-02-27 19:11:56 +0200

    Add files via upload

---
+++ 04. Advanced Querying/10. Book Search by Author/BookShop/StartUp.cs
@@ -0,0 +1,31 @@
+﻿namespace BookShop
+{
+    using Data;
+    using Initializer;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            using var db = new BookShopContext();
+            //DbInitializer.ResetDatabase(db);
+
+            string input = Console.ReadLine();
+            Console.WriteLine(GetBooksByAuthor(db, input));
+        }
+
+        public static string GetBooksByAuthor(BookShopContext context, string input)
+        {
+            var booksByAuthor = context
+                .Books
+                .Where(b => b.Author.LastName.ToLower().StartsWith(input.ToLower()))
+                .OrderBy(b => b.BookId)
+                .Select(b => $"{b.Title} ({b.Author.FirstName} {b.Author.LastName})")
+                .ToArray();
+
+            return String.Join(Environment.NewLine, booksByAuthor);
+        }
+    }
+}
+
+
