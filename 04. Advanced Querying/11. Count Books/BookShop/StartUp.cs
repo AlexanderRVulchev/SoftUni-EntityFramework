be9865b081f3885f0d41812780503d4ed0706755
Alexander Valchev <106471266+AlexanderRVulchev@users.noreply.github.com>
2023-02-27 19:13:25 +0200

    Add files via upload

---
+++ 04. Advanced Querying/11. Count Books/BookShop/StartUp.cs
@@ -0,0 +1,29 @@
+﻿namespace BookShop
+{
+    using Data;
+    using Initializer;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            using var db = new BookShopContext();
+            //DbInitializer.ResetDatabase(db);
+
+            int inputNum = int.Parse(Console.ReadLine());
+            Console.WriteLine(CountBooks(db, inputNum));
+        }
+
+        public static int CountBooks(BookShopContext context, int lengthCheck)
+        {
+            int countOfBooks = context
+                .Books
+                .Where(b => b.Title.Length > lengthCheck)
+                .Count();
+
+            return countOfBooks;
+        }
+    }
+}
+
+
