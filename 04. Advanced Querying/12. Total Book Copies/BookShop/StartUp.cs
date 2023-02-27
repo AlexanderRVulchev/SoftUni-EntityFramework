0aff5aca2bf19515c44cf89d38aecceef8cfc514
Alexander Valchev <106471266+AlexanderRVulchev@users.noreply.github.com>
2023-02-27 19:14:36 +0200

    Add files via upload

---
+++ 04. Advanced Querying/12. Total Book Copies/BookShop/StartUp.cs
@@ -0,0 +1,29 @@
+﻿namespace BookShop
+{
+    using Data;
+    using Initializer;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            using var db = new BookShopContext();
+            //DbInitializer.ResetDatabase(db);
+
+            Console.WriteLine(CountCopiesByAuthor(db));
+        }
+
+        public static string CountCopiesByAuthor(BookShopContext context)
+        {
+            string[] countCopiesByAuthor = context
+                .Authors
+                .OrderByDescending(a => a.Books.Sum(b => b.Copies))
+                .Select(a => $"{a.FirstName} {a.LastName} - {a.Books.Sum(b => b.Copies)}")
+                .ToArray();
+
+            return string.Join(Environment.NewLine, countCopiesByAuthor);
+        }
+    }
+}
+
+
