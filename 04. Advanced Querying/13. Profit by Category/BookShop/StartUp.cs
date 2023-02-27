c93760df7f9119474a0e599d9e8a2a1412788748
Alexander Valchev <106471266+AlexanderRVulchev@users.noreply.github.com>
2023-02-27 19:17:11 +0200

    Add files via upload

---
+++ 04. Advanced Querying/13. Profit by Category/BookShop/StartUp.cs
@@ -0,0 +1,30 @@
+﻿namespace BookShop
+{
+    using Data;
+    using Initializer;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            using var db = new BookShopContext();
+            //DbInitializer.ResetDatabase(db);
+
+            Console.WriteLine(GetTotalProfitByCategory(db));
+        }
+
+        public static string GetTotalProfitByCategory(BookShopContext context)
+        {
+            string[] totalProfitByCategory = context
+                .Categories                
+                .OrderByDescending(c => c.CategoryBooks.Sum(cb => cb.Book.Copies * cb.Book.Price))
+                .ThenBy(c => c.Name)
+                .Select(c => $"{c.Name} ${c.CategoryBooks.Sum(cb => cb.Book.Copies * cb.Book.Price):f2}")
+                .ToArray();
+
+            return string.Join(Environment.NewLine, totalProfitByCategory);
+        }
+    }
+}
+
+
