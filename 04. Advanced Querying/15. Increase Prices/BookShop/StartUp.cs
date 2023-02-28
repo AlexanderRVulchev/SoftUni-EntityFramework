dd57f65df4359a05519bc88a2209004b78a433ea
Alexander Valchev <106471266+AlexanderRVulchev@users.noreply.github.com>
2023-02-28 17:53:30 +0200

    Add files via upload

---
+++ 04. Advanced Querying/15. Increase Prices/BookShop/StartUp.cs
@@ -0,0 +1,32 @@
+﻿namespace BookShop
+{
+    using Data;
+    using Initializer;
+    
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            using var db = new BookShopContext();
+            //DbInitializer.ResetDatabase(db);
+            IncreasePrices(db);
+        }
+
+        public static void IncreasePrices(BookShopContext context)
+        {
+            var booksBefore2010 = context
+                .Books
+                .Where(b => b.ReleaseDate.Value.Year < 2010);
+
+            foreach (var book in booksBefore2010)
+            {
+                book.Price += 5;
+            }
+
+            context.SaveChanges();
+        }
+    }
+}
+
+
