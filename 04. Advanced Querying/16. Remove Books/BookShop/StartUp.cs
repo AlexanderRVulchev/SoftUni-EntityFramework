f2bfbfe9c3e32f8e0711a871d8f91a0dadfa5226
Alexander Valchev <106471266+AlexanderRVulchev@users.noreply.github.com>
2023-02-28 17:54:29 +0200

    Add files via upload

---
+++ 04. Advanced Querying/16. Remove Books/BookShop/StartUp.cs
@@ -0,0 +1,31 @@
+﻿namespace BookShop
+{
+    using Data;
+    using Initializer;
+
+    public class StartUp
+    {
+        public static void Main()
+        {
+            using var db = new BookShopContext();
+            //DbInitializer.ResetDatabase(db);
+
+            Console.WriteLine(RemoveBooks(db));
+        }
+
+        public static int RemoveBooks(BookShopContext context)
+        {
+            var booksToRemove = context
+                .Books
+                .Where(b => b.Copies < 4200)
+                .ToArray();
+
+            context.RemoveRange(booksToRemove);
+            context.SaveChanges();
+
+            return booksToRemove.Count();
+        }
+    }
+}
+
+
