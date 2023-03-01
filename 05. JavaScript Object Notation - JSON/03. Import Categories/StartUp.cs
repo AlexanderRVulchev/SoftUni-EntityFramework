7628d2584a88c367c9b992025d0d89e020bad5b8
Alexander Valchev <106471266+AlexanderRVulchev@users.noreply.github.com>
2023-03-01 18:13:22 +0200

    Add files via upload

---
+++ 05. JavaScript Object Notation - JSON/03. Import Categories/StartUp.cs
@@ -0,0 +1,55 @@
+﻿using Newtonsoft.Json;
+using ProductShop.Data;
+using ProductShop.Models;
+
+namespace ProductShop
+{
+    public class StartUp
+    {
+        public static void Main()
+        {
+            ProductShopContext context = new ProductShopContext();
+
+            context.Database.EnsureDeleted();            
+            context.Database.EnsureCreated();
+
+            string inputJsonUsers = File.ReadAllText(@"../../../Datasets/users.json");
+            ImportUsers(context, inputJsonUsers);
+
+            string inputJsonProducts = File.ReadAllText(@"../../../Datasets/products.json");
+            ImportProducts(context, inputJsonProducts);
+
+            string inputJsonCategories = File.ReadAllText(@"../../../Datasets/categories.json");
+            string output = ImportCategories(context, inputJsonCategories);            
+            Console.WriteLine(output);
+        }
+
+        public static string ImportUsers(ProductShopContext context, string inputJson)
+        {
+            List<User> users = JsonConvert.DeserializeObject<List<User>>(inputJson);
+            context.Users.AddRange(users);
+            context.SaveChanges();
+            return $"Successfully imported {users.Count}";
+        }
+
+        public static string ImportProducts(ProductShopContext context, string inputJson)
+        {
+            List<Product> products = JsonConvert.DeserializeObject<List<Product>>(inputJson);
+            context.Products.AddRange(products);
+            context.SaveChanges();
+            return $"Successfully imported {products.Count}";
+        }
+
+        public static string ImportCategories(ProductShopContext context, string inputJson)
+        {
+            List<Category> categories = JsonConvert
+                .DeserializeObject<List<Category>>(inputJson)
+                .Where(c => c.Name != null)
+                .ToList();
+
+            context.Categories.AddRange(categories);
+            context.SaveChanges();
+            return $"Successfully imported {categories.Count}";
+        }
+    }
+}
