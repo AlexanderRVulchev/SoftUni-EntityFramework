b85b47e376507cc64bf268a77a2dbd8ef3122206
Alexander Valchev <106471266+AlexanderRVulchev@users.noreply.github.com>
2023-03-01 18:07:50 +0200

    Add files via upload

---
+++ 05. JavaScript Object Notation - JSON/01. Import Users/StartUp.cs
@@ -0,0 +1,29 @@
+﻿using Newtonsoft.Json;
+using ProductShop.Data;
+using ProductShop.Models;
+
+namespace ProductShop
+{
+    public class StartUp
+    {
+        public static void Main()
+        {
+            ProductShopContext context = new ProductShopContext();
+
+            context.Database.EnsureDeleted();            
+            context.Database.EnsureCreated();            
+
+            string inputJson = File.ReadAllText(@"../../../Datasets/users.json");
+            string output = ImportUsers(context, inputJson);
+            Console.WriteLine(output);
+        }
+
+        public static string ImportUsers(ProductShopContext context, string inputJson)
+        {            
+            List<User> users = JsonConvert.DeserializeObject<List<User>>(inputJson);
+            context.Users.AddRange(users);
+            context.SaveChanges();
+            return $"Successfully imported {users.Count}";
+        }
+    }
+}
