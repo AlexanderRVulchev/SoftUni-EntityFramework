012387764d96e8db6a8ae84655ecd073148c668a
Alexander Valchev <106471266+AlexanderRVulchev@users.noreply.github.com>
2023-03-01 18:07:15 +0200

    Delete 05. JavaScript Object Notation - JSON/01. Import Users/ProductShop directory

--- 05. JavaScript Object Notation - JSON/01. Import Users/ProductShop/Data/Configuration.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace ProductShop.Data
-{
-    public static class Configuration
-    {
-        public const string ConnectionString =
-            @"Server=.;Database=ProductShop;Integrated Security=True";
-    }
-}
