ffc00380008aeb84b22f8efee48cfad6a6735f3c
Alexander Valchev <106471266+AlexanderRVulchev@users.noreply.github.com>
2023-03-02 19:00:28 +0200

    Add files via upload

---
+++ 05. JavaScript Object Notation - JSON/08. Export Users and Products/Models/Category.cs
@@ -0,0 +1,19 @@
+﻿namespace ProductShop.Models
+{
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+
+    public class Category
+    {
+        public Category()
+        {
+            CategoriesProducts = new List<CategoryProduct>();
+        }
+
+        public int Id { get; set; }
+                
+        public string Name { get; set; } = null!;
+
+        public ICollection<CategoryProduct> CategoriesProducts { get; set; }
+    }
+}
