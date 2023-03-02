602d232327a5106e43fbf2238b14b656c56198fb
Alexander Valchev <106471266+AlexanderRVulchev@users.noreply.github.com>
2023-03-02 19:01:19 +0200

    Add files via upload

---
+++ 05. JavaScript Object Notation - JSON/09. Import Suppliers/StartUp.cs
@@ -0,0 +1,31 @@
+﻿using CarDealer.Data;
+using CarDealer.Models;
+using Newtonsoft.Json;
+
+namespace CarDealer
+{
+    public class StartUp
+    {
+        public static void Main()
+        {
+            CarDealerContext context = new CarDealerContext();
+
+            context.Database.EnsureDeleted();
+            context.Database.EnsureCreated();
+
+            string suppliersFilePath = @"../../../Datasets/suppliers.json";
+            string suppliersJsonInput = File.ReadAllText(suppliersFilePath);
+
+            string output = ImportSuppliers(context, suppliersJsonInput);
+            Console.WriteLine(output);
+        }
+
+        public static string ImportSuppliers(CarDealerContext context, string inputJson)
+        {
+            var suppliers = JsonConvert.DeserializeObject<List<Supplier>>(inputJson);
+            context.Suppliers.AddRange(suppliers);
+            context.SaveChanges();
+            return $"Successfully imported {suppliers.Count}.";
+        }
+    }
+}
