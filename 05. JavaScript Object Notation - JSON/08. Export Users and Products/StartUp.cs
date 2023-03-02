353ba09bf1e1bce091fe6a24e4f9fb02c889c72b
Alexander Valchev <106471266+AlexanderRVulchev@users.noreply.github.com>
2023-03-02 19:00:11 +0200

    Add files via upload

---
+++ 05. JavaScript Object Notation - JSON/08. Export Users and Products/StartUp.cs
@@ -0,0 +1,98 @@
+﻿using Newtonsoft.Json;
+using ProductShop.Data;
+using ProductShop.Models;
+using System.Diagnostics;
+using System.Diagnostics.Metrics;
+using System.Xml.Linq;
+
+namespace ProductShop
+{
+    public class StartUp
+    {
+        public static void Main()
+        {
+            ProductShopContext context = new ProductShopContext();
+
+            context.Database.EnsureDeleted();
+            context.Database.EnsureCreated();
+            ImportData(context);
+
+            string jsonResult = GetUsersWithProducts(context);
+            Console.WriteLine(jsonResult);
+        }
+
+        public static void ImportData(ProductShopContext context)
+        {
+            string inputJsonUsers = File.ReadAllText(@"../../../Datasets/users.json");
+            string inputJsonProducts = File.ReadAllText(@"../../../Datasets/products.json");
+            string inputJsonCategories = File.ReadAllText(@"../../../Datasets/categories.json");
+            string inputJsonCategoriesProducts = File.ReadAllText(@"../../../Datasets/categories-products.json");
+
+            List<User>? users = JsonConvert
+                .DeserializeObject<List<User>>(inputJsonUsers);
+            context.Users.AddRange(users);
+            context.SaveChanges();
+
+            List<Product> products = JsonConvert
+                .DeserializeObject<List<Product>>(inputJsonProducts);
+            context.Products.AddRange(products);
+            context.SaveChanges();
+
+            List<Category> categories = JsonConvert
+                .DeserializeObject<List<Category>>(inputJsonCategories)
+                .Where(c => c.Name != null)
+                .ToList();
+            context.Categories.AddRange(categories);
+            context.SaveChanges();
+
+            List<CategoryProduct> categoryProducts = JsonConvert
+               .DeserializeObject<List<CategoryProduct>>(inputJsonCategoriesProducts);
+            context.CategoriesProducts.AddRange(categoryProducts);
+            context.SaveChanges();
+        }
+
+        public static string GetUsersWithProducts(ProductShopContext context)
+        {
+            var usersWithProducts = context
+                .Users
+                .Where(u => u.ProductsSold.Any(p => p.Buyer != null))
+                .OrderByDescending(u => u.ProductsSold
+                                         .Where(p => p.Buyer != null)
+                                         .Count())
+                .Select(u => new
+                {
+                    firstName = u.FirstName,
+                    lastName = u.LastName,
+                    age = u.Age,
+                    soldProducts = new
+                    {
+                        count = u.ProductsSold
+                            .Where(p => p.Buyer != null)
+                            .Count(),
+                        products = u.ProductsSold
+                            .Where(p => p.Buyer != null)
+                            .Select(p => new
+                            {
+                                name = p.Name,
+                                price = p.Price
+                            })
+                    }
+                })
+                .ToArray();
+
+            var usersInfo = new
+            {
+                usersCount = usersWithProducts.Count(),
+                users = usersWithProducts
+            };
+
+            string output = JsonConvert.SerializeObject(usersInfo, new JsonSerializerSettings
+            {
+                Formatting = Formatting.Indented,
+                NullValueHandling = NullValueHandling.Ignore,
+            });
+
+            return output;
+        }
+    }
+}
