5a12df36f11bf35b9c755c37570ee07025486344
Alexander Valchev <106471266+AlexanderRVulchev@users.noreply.github.com>
2023-03-04 10:22:40 +0200

    Add files via upload

---
+++ 05. JavaScript Object Notation - JSON/15. Export Cars From Make Toyota/StartUp.cs
@@ -0,0 +1,38 @@
+﻿using CarDealer.Data;
+using CarDealer.DTOs;
+using CarDealer.Models;
+using Newtonsoft.Json;
+
+
+namespace CarDealer
+{
+    public class StartUp
+    {
+        public static void Main()
+        {
+            CarDealerContext context = new CarDealerContext();
+
+            string jsonOutput = GetCarsFromMakeToyota(context);
+            string outputFilePath = @"../../../Results/toyota-cars.json";
+            File.WriteAllText(outputFilePath, jsonOutput);            
+        }
+
+        public static string GetCarsFromMakeToyota(CarDealerContext context)
+        {
+            var carsFromMakeToyota = context.Cars
+                .Where(c => c.Make == "Toyota")
+                .OrderBy(c => c.Model)
+                .ThenByDescending(c => c.TraveledDistance)
+                .Select(c => new 
+                {
+                    Id = c.Id,
+                    Make = c.Make,
+                    Model = c.Model,
+                    TraveledDistance = c.TraveledDistance
+                })
+                .ToArray();
+
+            return JsonConvert.SerializeObject(carsFromMakeToyota, Formatting.Indented);
+        }
+    }
+}
