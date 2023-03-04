b6cf0ef99240b016d0004977aaef3f354f0c0114
Alexander Valchev <106471266+AlexanderRVulchev@users.noreply.github.com>
2023-03-04 10:21:32 +0200

    Add files via upload

---
+++ 05. JavaScript Object Notation - JSON/14. Export Ordered Customers/StartUp.cs
@@ -0,0 +1,36 @@
+﻿using CarDealer.Data;
+using CarDealer.DTOs;
+using CarDealer.Models;
+using Newtonsoft.Json;
+using System.Globalization;
+
+namespace CarDealer
+{
+    public class StartUp
+    {
+        public static void Main()
+        {
+            CarDealerContext context = new CarDealerContext();
+
+            string jsonOutput = GetOrderedCustomers(context);
+            string outputFilePath = @"../../../Results/ordered-customers.json";
+            File.WriteAllText(outputFilePath, jsonOutput);
+        }
+
+        public static string GetOrderedCustomers(CarDealerContext context)
+        {
+            var orderedCustomers = context.Customers
+                .OrderBy(c => c.BirthDate)
+                .ThenBy(c => c.IsYoungDriver)
+                .Select(c => new
+                {
+                    Name = c.Name,
+                    BirthDate = c.BirthDate.ToString(@"dd/MM/yyyy", CultureInfo.InvariantCulture),
+                    IsYoungDriver = c.IsYoungDriver
+                })
+                .ToArray();
+
+            return JsonConvert.SerializeObject(orderedCustomers, Formatting.Indented);
+        }
+    }
+}
