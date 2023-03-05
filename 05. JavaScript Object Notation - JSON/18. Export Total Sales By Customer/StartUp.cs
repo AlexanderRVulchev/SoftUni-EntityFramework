cfa57939d6dd38964a0c8556dcaa7f67b791c534
Alexander Valchev <106471266+AlexanderRVulchev@users.noreply.github.com>
2023-03-05 09:31:22 +0200

    Add files via upload

---
+++ 05. JavaScript Object Notation - JSON/18. Export Total Sales By Customer/StartUp.cs
@@ -0,0 +1,47 @@
+﻿using CarDealer.Data;
+using CarDealer.DTOs;
+using CarDealer.Models;
+using Newtonsoft.Json;
+
+
+namespace CarDealer
+{
+    public class StartUp
+    {
+        public static void Main()
+        {
+            CarDealerContext context = new CarDealerContext();
+
+            string jsonOutput = GetTotalSalesByCustomer(context);
+            string outputFilePath = @"../../../Results/customers-total-sales.json";
+            File.WriteAllText(outputFilePath, jsonOutput);
+        }
+
+        public static string GetTotalSalesByCustomer(CarDealerContext context)
+        {
+            var customerSales = context.Customers
+                .Where(c => c.Sales.Any())
+                .Select(c => new
+                {
+                    fullName = c.Name,
+                    boughtCars = c.Sales.Count(),
+                    salePrices = c.Sales.SelectMany(x => x.Car.PartsCars.Select(x => x.Part.Price))
+                })
+                .ToArray();
+
+            var totalSalesByCustomer = customerSales.Select(t => new
+            {
+                t.fullName,
+                t.boughtCars,
+                spentMoney = t.salePrices.Sum()
+            })
+            .OrderByDescending(t => t.spentMoney)
+            .ThenByDescending(t => t.boughtCars)
+            .ToArray();
+
+            return JsonConvert.SerializeObject(totalSalesByCustomer, Formatting.Indented);
+        }
+
+
+    }
+}
