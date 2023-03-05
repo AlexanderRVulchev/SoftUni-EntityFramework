74dfeab799712c1455a39c653a83124cdeec2d46
Alexander Valchev <106471266+AlexanderRVulchev@users.noreply.github.com>
2023-03-05 09:30:41 +0200

    Add files via upload

---
+++ 05. JavaScript Object Notation - JSON/17. Export Cars With Their List Of Parts/Models/Car.cs
@@ -0,0 +1,23 @@
+﻿
+
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace CarDealer.Models
+{
+    public class Car
+    {
+        public int Id { get; set; }
+
+        public string Make { get; set; } = null!;
+
+        public string Model { get; set; } = null!;
+
+        [Column("TravelledDistance")]
+        public long TraveledDistance { get; set; }
+
+        public ICollection<Sale> Sales { get; set; } = new List<Sale>();    
+
+        public ICollection<PartCar> PartsCars { get; set; } = new List<PartCar>();
+    }
+}
