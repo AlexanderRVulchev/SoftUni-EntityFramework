0dff0fa5a754196c5c03012b4c175ef23a5499be
Alexander Valchev <106471266+AlexanderRVulchev@users.noreply.github.com>
2023-03-05 09:29:41 +0200

    Add files via upload

---
+++ 05. JavaScript Object Notation - JSON/16. Export Local Suppliers/StartUp.cs
@@ -0,0 +1,35 @@
+﻿using CarDealer.Data;
+using CarDealer.DTOs;
+using CarDealer.Models;
+using Newtonsoft.Json;
+
+
+namespace CarDealer
+{
+    public class StartUp
+    {
+        public static void Main()
+        {
+            CarDealerContext context = new CarDealerContext();
+
+            string jsonOutput = GetLocalSuppliers(context);
+            string outputFilePath = @"../../../Results/local-suppliers.json";
+            File.WriteAllText(outputFilePath, jsonOutput);            
+        }
+
+        public static string GetLocalSuppliers(CarDealerContext context)
+        {
+            var localSuppliers = context.Suppliers
+                .Where(s => !s.IsImporter)
+                .Select(s => new
+                {
+                    Id = s.Id,
+                    Name = s.Name,
+                    PartsCount = s.Parts.Count()
+                })
+                .ToArray();
+
+            return JsonConvert.SerializeObject(localSuppliers, Formatting.Indented);
+        }
+    }
+}
