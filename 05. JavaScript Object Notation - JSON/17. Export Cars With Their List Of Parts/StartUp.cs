8a47c9b3baec394faa5db54b74ad3d6a13103a72
Alexander Valchev <106471266+AlexanderRVulchev@users.noreply.github.com>
2023-03-05 09:30:23 +0200

    Add files via upload

---
+++ 05. JavaScript Object Notation - JSON/17. Export Cars With Their List Of Parts/StartUp.cs
@@ -0,0 +1,43 @@
+﻿using CarDealer.Data;
+using CarDealer.DTOs;
+using CarDealer.Models;
+using Newtonsoft.Json;
+
+
+namespace CarDealer
+{
+    public class StartUp
+    {
+        public static void Main()
+        {
+            CarDealerContext context = new CarDealerContext();
+
+            string jsonOutput = GetCarsWithTheirListOfParts(context);
+            string outputFilePath = @"../../../Results/cars-and-parts.json";
+            File.WriteAllText(outputFilePath, jsonOutput);            
+        }
+
+        public static string GetCarsWithTheirListOfParts(CarDealerContext context)
+        {
+            var carsAndParts = context.Cars
+                .Select(c => new
+                {
+                    car = new
+                    {
+                        c.Make,
+                        c.Model,
+                        c.TraveledDistance
+                    },
+                    parts = c.PartsCars
+                        .Select(p => new
+                        {
+                            p.Part.Name,
+                            Price = $"{p.Part.Price:f2}"
+                        })                        
+                })
+                .ToArray();
+
+            return JsonConvert.SerializeObject(carsAndParts, Formatting.Indented);
+        }
+    }
+}
