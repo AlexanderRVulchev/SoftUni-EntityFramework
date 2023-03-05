131af8d0c2e68782e48e29945e1005e0c0d3b4a4
Alexander Valchev <106471266+AlexanderRVulchev@users.noreply.github.com>
2023-03-05 09:32:07 +0200

    Add files via upload

---
+++ 05. JavaScript Object Notation - JSON/18. Export Total Sales By Customer/DTOs/CarDTO.cs
@@ -0,0 +1,25 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarDealer.DTOs
+{
+    public class CarDTO
+    {      
+
+        [JsonProperty("make")]
+        public string Make { get; set; } = null!;
+
+        [JsonProperty("model")]
+        public string Model { get; set; } = null!;
+
+        [JsonProperty("traveledDistance")]
+        public long TravelledDistance { get; set; }
+
+        [JsonProperty("partsId")]
+        public int[] PartsId { get; set; }
+    }
+}
