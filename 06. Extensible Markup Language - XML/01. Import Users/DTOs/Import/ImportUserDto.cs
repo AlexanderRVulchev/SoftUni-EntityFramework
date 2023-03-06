c58e6e7cda2fac99f8fd0026f36a6bc95126fbe1
Alexander Valchev <106471266+AlexanderRVulchev@users.noreply.github.com>
2023-03-06 18:09:33 +0200

    Add files via upload

---
+++ 06. Extensible Markup Language - XML/01. Import Users/DTOs/Import/ImportUserDto.cs
@@ -0,0 +1,17 @@
+﻿using System.Xml.Serialization;
+
+namespace ProductShop.DTOs.Import
+{
+    [XmlType("User")]
+    public class ImportUserDto
+    {
+        [XmlElement("firstName")]
+        public string FirstName { get; set; }
+
+        [XmlElement("lastName")]
+        public string LastName { get; set; }
+
+        [XmlElement("age")]
+        public int Age { get; set; }
+    }
+}
---
+++ 06. Extensible Markup Language - XML/01. Import Users/StartUp.cs
@@ -0,0 +1,56 @@
+﻿using ProductShop.Data;
+using ProductShop.DTOs.Import;
+using ProductShop.Models;
+using System.IO;
+using System.Xml;
+using System.Xml.Linq;
+using System.Xml.Serialization;
+
+namespace ProductShop
+{
+    public class StartUp
+    {
+        public static void Main()
+        {
+            var context = new ProductShopContext();
+            context.Database.EnsureDeleted();
+            context.Database.EnsureCreated();
+                        
+            string usersXmlFilePath = @"../../../Datasets/users.xml";
+            string usersXmlString = File.ReadAllText(usersXmlFilePath);
+            string output = ImportUsers(context, usersXmlString);
+            Console.WriteLine(output);
+        }
+
+        // This is a generic XML deserializer
+        private static T Deserialize<T>(string inputXml, string rootName)
+        {
+            XmlRootAttribute root = new XmlRootAttribute(rootName);
+            XmlSerializer serializer = new XmlSerializer(typeof(T), root);
+
+            using StringReader reader = new StringReader(inputXml);
+
+            T dtos = (T)serializer.Deserialize(reader);
+            return dtos;
+        }
+
+        public static string ImportUsers(ProductShopContext context, string inputXml)
+        {
+            var userDtos = Deserialize<ImportUserDto[]>(inputXml, "Users");
+
+            User[] users = userDtos
+                .Select(s => new User()
+                {
+                    FirstName = s.FirstName,
+                    LastName = s.LastName,
+                    Age = s.Age
+                })
+                .ToArray();
+
+            context.Users.AddRange(users);
+            context.SaveChanges();
+
+            return $"Successfully imported {users.Count()}";
+        }
+    }
+}
