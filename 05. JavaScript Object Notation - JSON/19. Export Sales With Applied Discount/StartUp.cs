7bdc352ec6f37a59ad41c2f39130504af5006982
Alexander Valchev <106471266+AlexanderRVulchev@users.noreply.github.com>
2023-03-06 17:58:07 +0200

    Add files via upload

---
+++ 05. JavaScript Object Notation - JSON/19. Export Sales With Applied Discount/StartUp.cs
@@ -0,0 +1,43 @@
+﻿using CarDealer.Data;
+using CarDealer.DTOs;
+using CarDealer.Models;
+using Newtonsoft.Json;
+using System.Diagnostics;
+
+
+namespace CarDealer
+{
+    public class StartUp
+    {
+        public static void Main()
+        {
+            CarDealerContext context = new CarDealerContext();
+
+            string jsonOutput = GetSalesWithAppliedDiscount(context);
+            string outputFilePath = @"../../../Results/sales-discounts.json";
+            File.WriteAllText(outputFilePath, jsonOutput);
+        }
+
+        public static string GetSalesWithAppliedDiscount(CarDealerContext context)
+        {
+            var salesWithDiscount = context.Sales
+                .Take(10)
+                .Select(s => new
+                {
+                    car = new
+                    {
+                        s.Car.Make,
+                        s.Car.Model,
+                        s.Car.TraveledDistance
+                    },
+                    customerName = s.Customer.Name,
+                    discount = $"{s.Discount:f2}",
+                    price = $"{s.Car.PartsCars.Sum(p => p.Part.Price):f2}",
+                    priceWithDiscount = $"{s.Car.PartsCars.Sum(p => p.Part.Price) * (1 - s.Discount / 100):f2}"
+                })                
+                .ToArray();
+
+            return JsonConvert.SerializeObject(salesWithDiscount, Formatting.Indented);
+        }
+    }
+}
