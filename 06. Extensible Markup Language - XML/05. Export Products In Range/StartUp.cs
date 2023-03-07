9d66a85efe9005ec2bec31761dbefecd17790fbf
Alexander Valchev <106471266+AlexanderRVulchev@users.noreply.github.com>
2023-03-07 18:35:15 +0200

    Add files via upload

---
+++ 06. Extensible Markup Language - XML/05. Export Products In Range/StartUp.cs
@@ -0,0 +1,51 @@
+﻿using ProductShop.Data;
+using ProductShop.DTOs.Export;
+using System.Text;
+using System.Xml.Serialization;
+
+namespace ProductShop
+{
+    public class StartUp
+    {
+        public static void Main()
+        {
+            var context = new ProductShopContext();
+
+            string xmlOutput = GetProductsInRange(context);
+            File.WriteAllText(@"../../../Results/products-in-range.xml", xmlOutput);
+        }
+
+        // this is a generic XML Serializer
+        private static string Serializer<T>(T dataTransferObjects, string xmlRootAttributeName)
+        {
+            XmlSerializer serializer = new XmlSerializer(typeof(T), new XmlRootAttribute(xmlRootAttributeName));
+
+            StringBuilder sb = new StringBuilder();
+            using var write = new StringWriter(sb);
+
+            XmlSerializerNamespaces xmlNamespaces = new XmlSerializerNamespaces();
+            xmlNamespaces.Add(string.Empty, string.Empty);
+
+            serializer.Serialize(write, dataTransferObjects, xmlNamespaces);
+
+            return sb.ToString();
+        }
+
+        public static string GetProductsInRange(ProductShopContext context)
+        {
+            var productsInRange = context.Products
+                .Where(p => p.Price >= 500 && p.Price <= 1000)
+                .OrderBy(p => p.Price)
+                .Take(10)
+                .Select(p => new ExportProductsInRangeDto()
+                {
+                    Price = p.Price,
+                    Name = p.Name,
+                    Buyer = p.Buyer.FirstName + " " + p.Buyer.LastName
+                })
+                .ToArray();
+
+            return Serializer<ExportProductsInRangeDto[]>(productsInRange, "Products");
+        }
+    }
+}
