e602a3c5b7df02656d36aa98a3f27ae445147f49
Alexander Valchev <106471266+AlexanderRVulchev@users.noreply.github.com>
2023-03-08 16:34:54 +0200

    Add files via upload

---
+++ 06. Extensible Markup Language - XML/07. Export Categories By Products Count/StartUp.cs
@@ -0,0 +1,52 @@
+﻿using ProductShop.Data;
+using ProductShop.DTOs.Export;
+using System.Text;
+using System.Xml.Serialization;
+
+namespace ProductShop
+{
+    public class StartUp
+    {
+        public static void Main()
+        {
+            var context = new ProductShopContext();
+
+            string xmlOutput = GetCategoriesByProductsCount(context);
+            File.WriteAllText(@"../../../Results/categories-by-products.xml", xmlOutput);
+        }
+
+        // this is a generic XML Serializer
+        private static string Serializer<T>(T dataTransferObjects, string xmlRootAttributeName)
+        {
+            XmlSerializer serializer = new XmlSerializer(typeof(T), new XmlRootAttribute(xmlRootAttributeName));
+
+            StringBuilder sb = new StringBuilder();
+            using var write = new StringWriter(sb);
+
+            XmlSerializerNamespaces xmlNamespaces = new XmlSerializerNamespaces();
+            xmlNamespaces.Add(string.Empty, string.Empty);
+
+            serializer.Serialize(write, dataTransferObjects, xmlNamespaces);
+
+            return sb.ToString();
+        }
+
+        public static string GetCategoriesByProductsCount(ProductShopContext context)
+        {
+            ExportCategoriesByProductsCountDto[] categoriesByProductsCount = context
+                .Categories
+                .Select(c => new ExportCategoriesByProductsCountDto()
+                {
+                    Name = c.Name,
+                    Count = c.CategoryProducts.Count(),
+                    AveragePrice = c.CategoryProducts.Average(p => p.Product.Price),
+                    TotalRevenue = c.CategoryProducts.Sum(p => p.Product.Price)
+                })
+                .OrderByDescending(c => c.Count)
+                .ThenBy(c => c.TotalRevenue)
+                .ToArray();
+
+            return Serializer<ExportCategoriesByProductsCountDto[]>(categoriesByProductsCount, "Categories");
+        }
+    }
+}
