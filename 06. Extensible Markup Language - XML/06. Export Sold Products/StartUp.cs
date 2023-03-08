e16d3f3229d961d2b06d758f75881f15a3982d84
Alexander Valchev <106471266+AlexanderRVulchev@users.noreply.github.com>
2023-03-08 16:33:47 +0200

    Add files via upload

---
+++ 06. Extensible Markup Language - XML/06. Export Sold Products/StartUp.cs
@@ -0,0 +1,57 @@
+﻿using ProductShop.Data;
+using ProductShop.DTOs.Export;
+using System.Text;
+using System.Xml.Serialization;
+
+namespace ProductShop
+{
+    public class StartUp
+    {
+        public static void Main()
+        {
+            var context = new ProductShopContext();
+
+            string xmlOutput = GetSoldProducts(context);
+            File.WriteAllText(@"../../../Results/users-sold-products.xml.xml", xmlOutput);
+        }
+
+        // this is a generic XML Serializer
+        private static string Serializer<T>(T dataTransferObjects, string xmlRootAttributeName)
+        {
+            XmlSerializer serializer = new XmlSerializer(typeof(T), new XmlRootAttribute(xmlRootAttributeName));
+
+            StringBuilder sb = new StringBuilder();
+            using var write = new StringWriter(sb);
+
+            XmlSerializerNamespaces xmlNamespaces = new XmlSerializerNamespaces();
+            xmlNamespaces.Add(string.Empty, string.Empty);
+
+            serializer.Serialize(write, dataTransferObjects, xmlNamespaces);
+
+            return sb.ToString();
+        }
+
+        public static string GetSoldProducts(ProductShopContext context)
+        {
+            ExportSoldProductsDto[] usersSoldProducts = context.Users
+                .Where(u => u.ProductsSold.Any())
+                .OrderBy(u => u.LastName)
+                .ThenBy(u => u.FirstName)
+                .Take(5)
+                .Select(u => new ExportSoldProductsDto()
+                {
+                    FirstName = u.FirstName,
+                    LastName = u.LastName,
+                    SoldProducts = u.ProductsSold.Select(p => new ProductDto()
+                    {
+                        Name = p.Name,
+                        Price = p.Price
+                    })
+                    .ToArray()
+                })
+                .ToArray();
+
+            return Serializer<ExportSoldProductsDto[]>(usersSoldProducts, "Users");
+        }
+    }
+}
