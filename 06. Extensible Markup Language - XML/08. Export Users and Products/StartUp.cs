10f51a570bb4f08b9a1d25d9b84db6cee7bcf5fd
Alexander Valchev <106471266+AlexanderRVulchev@users.noreply.github.com>
2023-03-09 19:24:08 +0200

    Add files via upload

---
+++ 06. Extensible Markup Language - XML/08. Export Users and Products/StartUp.cs
@@ -0,0 +1,69 @@
+﻿using ProductShop.Data;
+using ProductShop.DTOs.Export;
+using System.Text;
+using System.Xml.Serialization;
+
+namespace ProductShop
+{
+    public class StartUp
+    {
+        public static void Main()
+        {
+            var context = new ProductShopContext();
+
+            string xmlOutput = GetUsersWithProducts(context);
+            File.WriteAllText(@"../../../Results/users-and-products.xml", xmlOutput);
+        }
+
+        // this is a generic XML Serializer
+        private static string Serializer<T>(T dataTransferObjects, string xmlRootAttributeName)
+        {
+            XmlSerializer serializer = new XmlSerializer(typeof(T), new XmlRootAttribute(xmlRootAttributeName));
+
+            StringBuilder sb = new StringBuilder();
+            using var write = new StringWriter(sb);
+
+            XmlSerializerNamespaces xmlNamespaces = new XmlSerializerNamespaces();
+            xmlNamespaces.Add(string.Empty, string.Empty);
+
+            serializer.Serialize(write, dataTransferObjects, xmlNamespaces);
+
+            return sb.ToString();
+        }
+
+        public static string GetUsersWithProducts(ProductShopContext context)
+        {
+            var usersInfo = context
+                .Users
+                .Where(u => u.ProductsSold.Any())
+                .OrderByDescending(u => u.ProductsSold.Count)                
+                .Select(u => new UserInfo()
+                {
+                    FirstName = u.FirstName,
+                    LastName = u.LastName,
+                    Age = u.Age,
+                    SoldProducts = new SoldProductsCount()
+                    {
+                        Count = u.ProductsSold.Count,
+                        Products = u.ProductsSold.Select(p => new SoldProduct()
+                        {
+                            Name = p.Name,
+                            Price = p.Price
+                        })
+                        .OrderByDescending(p => p.Price)
+                        .ToArray()
+                    }
+                })
+                .Take(10)
+                .ToArray();
+
+            ExportUserCountDto exportUserCountDto = new ExportUserCountDto()
+            {
+                Count = context.Users.Count(u => u.ProductsSold.Any()),
+                Users = usersInfo
+            };
+
+            return Serializer<ExportUserCountDto>(exportUserCountDto, "Users");
+        }
+    }
+}
