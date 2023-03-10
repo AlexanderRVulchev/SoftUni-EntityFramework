9cd9bc4c5c8e536957a98490ca888c017e96ec9d
Alexander Valchev <106471266+AlexanderRVulchev@users.noreply.github.com>
2023-03-10 21:54:04 +0200

    Add files via upload

---
+++ 06. Extensible Markup Language - XML/12. Import Customers/StartUp.cs
@@ -0,0 +1,149 @@
+﻿using CarDealer.Data;
+using CarDealer.DTOs.Import;
+using CarDealer.Models;
+using System.Xml.Serialization;
+
+namespace CarDealer
+{
+    public class StartUp
+    {
+        public static void Main()
+        {
+            var context = new CarDealerContext();
+
+            context.Database.EnsureDeleted();
+            context.Database.EnsureCreated();
+
+            string suppliersFilePath = @"../../../Datasets/suppliers.xml";
+            string suppliersXml = File.ReadAllText(suppliersFilePath);
+            ImportSuppliers(context, suppliersXml);
+
+            string partsFilePath = @"../../../Datasets/parts.xml";
+            string partsXml = File.ReadAllText(partsFilePath);
+            ImportParts(context, partsXml);
+
+            string carsFilePath = @"../../../Datasets/cars.xml";
+            string carsXml = File.ReadAllText(carsFilePath);
+            ImportCars(context, carsXml);
+
+            string customersFilePath = @"../../../Datasets/customers.xml";
+            string customersXml = File.ReadAllText(customersFilePath);
+            string output = ImportCustomers(context, customersXml);
+
+            Console.WriteLine(output);
+        }
+
+        // This is a generic XML deserializer
+        private static T Deserializer<T>(string inputXml, string rootName)
+        {
+            XmlRootAttribute root = new XmlRootAttribute(rootName);
+            XmlSerializer serializer = new XmlSerializer(typeof(T), root);
+
+            using StringReader reader = new StringReader(inputXml);
+
+            T dtos = (T)serializer.Deserialize(reader);
+            return dtos;
+        }
+
+        public static string ImportSuppliers(CarDealerContext context, string inputXml)
+        {
+            ImportSuppliersDto[] suppliersDtos = Deserializer<ImportSuppliersDto[]>(inputXml, "Suppliers");
+
+            Supplier[] suppliers = suppliersDtos
+                .Select(s => new Supplier()
+                {
+                    Name = s.Name,
+                    IsImporter = s.IsImporter
+                })
+                .ToArray();
+
+            context.Suppliers.AddRange(suppliers);
+            context.SaveChanges();
+
+            return $"Successfully imported {suppliers.Count()}";
+        }
+
+        public static string ImportParts(CarDealerContext context, string inputXml)
+        {
+            ImportPartsDto[] partsDtos = Deserializer<ImportPartsDto[]>(inputXml, "Parts");
+
+            int[] supplierIds = context.Suppliers.Select(s => s.Id).ToArray();
+
+            Part[] parts = partsDtos
+                .Where(p => supplierIds.Contains(p.SupplierId))
+                .Select(p => new Part()
+                {
+                    Name = p.Name,
+                    Price = p.Price,
+                    Quantity = p.Quantity,
+                    SupplierId = p.SupplierId
+                })
+                .ToArray();
+
+            context.Parts.AddRange(parts);
+            context.SaveChanges();
+
+            return $"Successfully imported {parts.Count()}";
+        }
+
+        public static string ImportCars(CarDealerContext context, string inputXml)
+        {
+            ImportCarsDto[] carsDtos = Deserializer<ImportCarsDto[]>(inputXml, "Cars");
+
+            List<Car> cars = new List<Car>();
+            List<PartCar> partCars = new List<PartCar>();
+            int[] allPartIds = context.Parts.Select(p => p.Id).ToArray();
+            int carId = 1;
+
+            foreach (var dto in carsDtos)
+            {
+                Car car = new Car()
+                {
+                    Make = dto.Make,
+                    Model = dto.Model,
+                    TravelledDistance = dto.TraveledDistance
+                };
+
+                cars.Add(car);
+
+                foreach (int partId in dto.Parts
+                    .Where(p => allPartIds.Contains(p.PartId))
+                    .Select(p => p.PartId)
+                    .Distinct())
+                {
+                    PartCar partCar = new PartCar()
+                    {
+                        CarId = carId,
+                        PartId = partId
+                    };
+                    partCars.Add(partCar);
+                }
+                carId++;
+            }
+
+            context.Cars.AddRange(cars);            
+            context.PartsCars.AddRange(partCars);
+            context.SaveChanges();
+
+            return $"Successfully imported {cars.Count}";
+        }
+
+        public static string ImportCustomers(CarDealerContext context, string inputXml)
+        {
+            ImportCustomersDto[] customersDtos = Deserializer<ImportCustomersDto[]>(inputXml, "Customers");
+
+            Customer[] customers = customersDtos.Select(c => new Customer()
+            {
+                Name = c.Name,
+                BirthDate = c.BirthDate,
+                IsYoungDriver = c.isYoungDriver
+            })
+            .ToArray();
+
+            context.Customers.AddRange(customers);
+            context.SaveChanges();
+
+            return $"Successfully imported {customers.Count()}";
+        }
+    }
+}
