2f9647159eec4b8e6768d7ddac987270205f20e8
Alexander Valchev <106471266+AlexanderRVulchev@users.noreply.github.com>
2023-03-11 11:18:27 +0200

    Add files via upload

---
+++ 06. Extensible Markup Language - XML/15. Export Cars From Make BMW/DTOs/Export/CarsFromMakeBmwDto.cs
@@ -0,0 +1,17 @@
+﻿using System.Xml.Serialization;
+
+namespace CarDealer.DTOs.Export
+{
+    [XmlType("car")]
+    public class CarsFromMakeBmwDto
+    {        
+        [XmlAttribute("id")]
+        public int Id { get; set; }
+
+        [XmlAttribute("model")]
+        public string Model { get; set; }
+
+        [XmlAttribute("traveled-distance")]
+        public long TraveledDistance { get; set; }
+    }
+}
---
+++ 06. Extensible Markup Language - XML/15. Export Cars From Make BMW/StartUp.cs
@@ -0,0 +1,51 @@
+﻿using CarDealer.Data;
+using CarDealer.DTOs.Export;
+using System.Text;
+using System.Xml.Serialization;
+
+namespace CarDealer
+{
+    public class StartUp
+    {
+        public static void Main()
+        {
+            var context = new CarDealerContext();
+
+            string xmlOutput = GetCarsFromMakeBmw(context);
+            File.WriteAllText(@"../../../Results/bmw-cars.xml", xmlOutput);
+        }
+
+        // this is a generic XML Serializer
+        private static string Serializer<T>(T dataTransferObjects, string xmlRootAttributeName)
+        {
+            XmlSerializer serializer = new XmlSerializer(typeof(T), new XmlRootAttribute(xmlRootAttributeName));
+
+            StringBuilder sb = new StringBuilder();
+            using var write = new StringWriter(sb);
+
+            XmlSerializerNamespaces xmlNamespaces = new XmlSerializerNamespaces();
+            xmlNamespaces.Add(string.Empty, string.Empty);
+
+            serializer.Serialize(write, dataTransferObjects, xmlNamespaces);
+
+            return sb.ToString();
+        }
+
+        public static string GetCarsFromMakeBmw(CarDealerContext context)
+        {
+            CarsFromMakeBmwDto[] carsDtos = context.Cars
+                .Where(c => c.Make == "BMW")
+                .Select(c => new CarsFromMakeBmwDto()
+                {
+                    Id = c.Id,
+                    Model = c.Model,
+                    TraveledDistance = c.TraveledDistance
+                })
+                .OrderBy(c => c.Model)
+                .ThenByDescending(c => c.TraveledDistance)
+                .ToArray();
+
+            return Serializer<CarsFromMakeBmwDto[]>(carsDtos, "cars");
+        }
+    }
+}
