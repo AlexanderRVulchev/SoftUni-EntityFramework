4459e86d5c609884b8055e6a8c68ac6ca8a5695f
Alexander Valchev <106471266+AlexanderRVulchev@users.noreply.github.com>
2023-03-11 11:18:51 +0200

    Add files via upload

---
+++ 06. Extensible Markup Language - XML/16. Export Local Suppliers/DTOs/Export/LocalSuppliersDto.cs
@@ -0,0 +1,17 @@
+﻿using System.Xml.Serialization;
+
+namespace CarDealer.DTOs.Export
+{
+    [XmlType("supplier")]
+    public class LocalSuppliersDto
+    {
+        [XmlAttribute("id")]
+        public int Id { get; set; }
+
+        [XmlAttribute("name")]
+        public string Name { get; set; }
+
+        [XmlAttribute("parts-count")]
+        public int PartsCount { get; set; }
+    }
+}
---
+++ 06. Extensible Markup Language - XML/16. Export Local Suppliers/StartUp.cs
@@ -0,0 +1,50 @@
+﻿using CarDealer.Data;
+using CarDealer.DTOs.Export;
+using System.Text;
+using System.Xml.Serialization;
+
+namespace CarDealer
+{
+    public class StartUp
+    {
+        public static void Main()
+        {
+            var context = new CarDealerContext();
+
+            string xmlOutput = GetLocalSuppliers(context);
+            File.WriteAllText(@"../../../Results/local-suppliers.xml", xmlOutput);
+        }
+
+        // this is a generic XML Serializer
+        private static string Serializer<T>(T dataTransferObjects, string xmlRootAttributeName)
+        {
+            XmlSerializer serializer = new XmlSerializer(typeof(T), new XmlRootAttribute(xmlRootAttributeName));
+
+            StringBuilder sb = new StringBuilder();
+            using var write = new StringWriter(sb);
+
+            XmlSerializerNamespaces xmlNamespaces = new XmlSerializerNamespaces();
+            xmlNamespaces.Add(string.Empty, string.Empty);
+
+            serializer.Serialize(write, dataTransferObjects, xmlNamespaces);
+
+            return sb.ToString();
+        }
+
+        public static string GetLocalSuppliers(CarDealerContext context)
+        {
+            LocalSuppliersDto[] localSuppliersDtos = context.Suppliers
+                .Where(s => !s.IsImporter)
+                .Select(s => new LocalSuppliersDto()
+                {
+                    Name = s.Name,
+                    Id = s.Id,
+                    PartsCount = s.Parts.Count()
+                })                
+                .ToArray();
+
+            return Serializer<LocalSuppliersDto[]>(localSuppliersDtos, "suppliers");
+        }
+
+    }
+}
