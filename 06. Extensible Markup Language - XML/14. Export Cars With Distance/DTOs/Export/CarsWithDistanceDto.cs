9ff565c9d4db6447afc06dd62da954ee36a9f620
Alexander Valchev <106471266+AlexanderRVulchev@users.noreply.github.com>
2023-03-11 11:18:03 +0200

    Add files via upload

---
+++ 06. Extensible Markup Language - XML/14. Export Cars With Distance/DTOs/Export/CarsWithDistanceDto.cs
@@ -0,0 +1,17 @@
+﻿using System.Xml.Serialization;
+
+namespace CarDealer.DTOs.Export
+{
+    [XmlType("car")]
+    public class CarsWithDistanceDto
+    {        
+        [XmlElement("make")]
+        public string Make { get; set; }
+
+        [XmlElement("model")]
+        public string Model { get; set; }
+
+        [XmlElement("traveled-distance")]
+        public long TravelledDistance { get; set; }
+    }
+}
---
+++ 06. Extensible Markup Language - XML/14. Export Cars With Distance/StartUp.cs
@@ -0,0 +1,52 @@
+﻿using CarDealer.Data;
+using CarDealer.DTOs.Export;
+using System.Text;
+using System.Xml.Serialization;
+
+namespace CarDealer
+{
+    public class StartUp
+    {
+        public static void Main()
+        {
+            var context = new CarDealerContext();
+
+            string xmlOutput = GetCarsWithDistance(context);            
+            File.WriteAllText(@"../../../Results/cars.xml", xmlOutput);
+        }
+
+        // this is a generic XML Serializer
+        private static string Serializer<T>(T dataTransferObjects, string xmlRootAttributeName)
+        {
+            XmlSerializer serializer = new XmlSerializer(typeof(T), new XmlRootAttribute(xmlRootAttributeName));
+
+            StringBuilder sb = new StringBuilder();
+            using var write = new StringWriter(sb);
+
+            XmlSerializerNamespaces xmlNamespaces = new XmlSerializerNamespaces();
+            xmlNamespaces.Add(string.Empty, string.Empty);
+
+            serializer.Serialize(write, dataTransferObjects, xmlNamespaces);
+
+            return sb.ToString();
+        }
+
+        public static string GetCarsWithDistance(CarDealerContext context)
+        {
+            CarsWithDistanceDto[] carsDtos = context.Cars
+                .Where(c => c.TraveledDistance > 2_000_000)
+                .OrderBy(c => c.Make)
+                .ThenBy(c => c.Model)
+                .Take(10)
+                .Select(c => new CarsWithDistanceDto()
+                {
+                    Make = c.Make,
+                    Model = c.Model,
+                    TravelledDistance = c.TraveledDistance,
+                })
+                .ToArray();
+
+            return Serializer<CarsWithDistanceDto[]>(carsDtos, "cars");
+        }
+    }
+}
