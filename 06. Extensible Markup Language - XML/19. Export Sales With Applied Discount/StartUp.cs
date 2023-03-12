9a21cc04ee1f27e225197d2a8408f8f57b07a6bc
Alexander Valchev <106471266+AlexanderRVulchev@users.noreply.github.com>
2023-03-12 15:06:29 +0200

    Add files via upload

---
+++ 06. Extensible Markup Language - XML/19. Export Sales With Applied Discount/StartUp.cs
@@ -0,0 +1,58 @@
+﻿using CarDealer.Data;
+using CarDealer.DTOs.Export;
+using System.Text;
+using System.Xml.Serialization;
+using System.Linq;
+using System.Xml;
+
+namespace CarDealer
+{
+    public class StartUp
+    {
+        public static void Main()
+        {
+            var context = new CarDealerContext();
+
+            string xmlOutput = GetSalesWithAppliedDiscount(context);
+            File.WriteAllText(@"../../../Results/sales-discounts.xml", xmlOutput);
+        }
+
+        // this is a generic XML Serializer
+        private static string Serializer<T>(T dataTransferObjects, string xmlRootAttributeName)
+        {
+            XmlSerializer serializer = new XmlSerializer(typeof(T), new XmlRootAttribute(xmlRootAttributeName));
+
+            StringBuilder sb = new StringBuilder();
+            using var write = new StringWriter(sb);
+
+            XmlSerializerNamespaces xmlNamespaces = new XmlSerializerNamespaces();
+            xmlNamespaces.Add(string.Empty, string.Empty);
+
+            serializer.Serialize(write, dataTransferObjects, xmlNamespaces);
+
+            return sb.ToString();
+        }
+
+        public static string GetSalesWithAppliedDiscount(CarDealerContext context)
+        {
+            SalesWithAppliedDiscountDto[] salesDtos = context
+                .Sales
+                .Select(s => new SalesWithAppliedDiscountDto()
+                {
+                    SingleCar = new SingleCar()
+                    {
+                        Make = s.Car.Make,
+                        Model = s.Car.Model,
+                        TraveledDistance = s.Car.TraveledDistance
+                    },
+                    Discount = (int)s.Discount,
+                    CustomerName = s.Customer.Name,
+                    Price = s.Car.PartsCars.Sum(p => p.Part.Price),
+                    PriceWithDiscount = Math.Round((double)(s.Car.PartsCars.Sum(p => p.Part.Price) * (1 - (s.Discount / 100))), 4)
+                })
+                .ToArray();
+
+            return Serializer<SalesWithAppliedDiscountDto[]>(salesDtos, "sales");
+        }
+    }
+}
