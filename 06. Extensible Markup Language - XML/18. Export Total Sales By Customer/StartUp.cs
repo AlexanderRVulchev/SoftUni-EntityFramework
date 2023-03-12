be8ecc52f5574a5d7b7b701bfaa0d13e4d71746d
Alexander Valchev <106471266+AlexanderRVulchev@users.noreply.github.com>
2023-03-12 15:05:32 +0200

    Add files via upload

---
+++ 06. Extensible Markup Language - XML/18. Export Total Sales By Customer/StartUp.cs
@@ -0,0 +1,66 @@
+﻿using CarDealer.Data;
+using CarDealer.DTOs.Export;
+using System.Text;
+using System.Xml.Serialization;
+using System.Linq;
+using System.Xml;
+
+namespace CarDealer
+{
+    public class StartUp
+    {
+        public static void Main()
+        {
+            var context = new CarDealerContext();
+
+            string xmlOutput = GetTotalSalesByCustomer(context);
+            File.WriteAllText(@"../../../Results/customers-total-sales.xml", xmlOutput);
+        }
+
+        // this is a generic XML Serializer
+        private static string Serializer<T>(T dataTransferObjects, string xmlRootAttributeName)
+        {
+            XmlSerializer serializer = new XmlSerializer(typeof(T), new XmlRootAttribute(xmlRootAttributeName));
+
+            StringBuilder sb = new StringBuilder();
+            using var write = new StringWriter(sb);
+
+            XmlSerializerNamespaces xmlNamespaces = new XmlSerializerNamespaces();
+            xmlNamespaces.Add(string.Empty, string.Empty);
+
+            serializer.Serialize(write, dataTransferObjects, xmlNamespaces);
+
+            return sb.ToString();
+        }
+
+        public static string GetTotalSalesByCustomer(CarDealerContext context)
+        {
+            var tempDto = context.Customers
+                .Where(c => c.Sales.Any())                              
+                .Select(c => new
+                {
+                    FullName = c.Name,
+                    BoughtCars = c.Sales.Count(),
+                    SalesInfo = c.Sales.Select(s => new
+                    {
+                        Prices = c.IsYoungDriver 
+                            ? s.Car.PartsCars.Sum(p => Math.Round((double)p.Part.Price * 0.95, 2)) 
+                            : s.Car.PartsCars.Sum(p => (double)p.Part.Price)                      
+                    }).ToArray(),                                        
+                })
+                .ToArray();
+
+            TotalSalesByCustomerDto[] totalSalesDtos = tempDto
+                .OrderByDescending(t => t.SalesInfo.Sum(s => s.Prices))
+                .Select(t => new TotalSalesByCustomerDto()
+                {
+                    FullName = t.FullName,
+                    BoughtCars = t.BoughtCars,
+                    SpentMoney = t.SalesInfo.Sum(s => s.Prices).ToString("f2")                
+                })                
+                .ToArray();
+
+            return Serializer<TotalSalesByCustomerDto[]>(totalSalesDtos, "customers");
+        }
+    }
+}
