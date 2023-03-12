c009a25565ade4830a112611f806c2c6e37bb2f8
Alexander Valchev <106471266+AlexanderRVulchev@users.noreply.github.com>
2023-03-12 15:04:56 +0200

    Add files via upload

---
+++ 06. Extensible Markup Language - XML/17. Export Cars With Their List Of Parts/DTOs/Export/CarsWithPartsListDto.cs
@@ -0,0 +1,30 @@
+﻿using System.Xml.Serialization;
+
+namespace CarDealer.DTOs.Export
+{
+    [XmlType("car")]
+    public class CarsWithPartsListDto
+    {        
+        [XmlAttribute("make")]
+        public string Make { get; set; }
+
+        [XmlAttribute("model")]
+        public string Model { get; set; }
+
+        [XmlAttribute("traveled-distance")]
+        public long TraveledDistance { get; set; }
+
+        [XmlArray("parts")]
+        public CarPartsDto[] CarParts { get; set; }
+    }
+
+    [XmlType("part")]
+    public class CarPartsDto
+    {
+        [XmlAttribute("name")]
+        public string Name { get; set; }
+
+        [XmlAttribute("price")]
+        public decimal Price { get; set; }
+    }
+}
