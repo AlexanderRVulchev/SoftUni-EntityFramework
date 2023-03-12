60f16ecf388bb2326c24d791213c3edc27dbf904
Alexander Valchev <106471266+AlexanderRVulchev@users.noreply.github.com>
2023-03-12 15:05:52 +0200

    Add files via upload

---
+++ 06. Extensible Markup Language - XML/18. Export Total Sales By Customer/DTOs/Export/TotalSalesByCustomerDto.cs
@@ -0,0 +1,17 @@
+﻿using System.Xml.Serialization;
+
+namespace CarDealer.DTOs.Export
+{
+    [XmlType("customer")]
+    public class TotalSalesByCustomerDto
+    {        
+        [XmlAttribute("full-name")]
+        public string FullName { get; set; }
+
+        [XmlAttribute("bought-cars")]
+        public int BoughtCars { get; set; }
+
+        [XmlAttribute("spent-money")]
+        public string SpentMoney { get; set; }
+    }
+}
---
+++ 06. Extensible Markup Language - XML/18. Export Total Sales By Customer/DTOs/Import/ImportSuppliersDto.cs
@@ -0,0 +1,14 @@
+﻿using System.Xml.Serialization;
+
+namespace CarDealer.DTOs.Import
+{
+    [XmlType("Supplier")]
+    public class ImportSuppliersDto
+    {
+        [XmlElement("name")]
+        public string Name { get; set; }
+
+        [XmlElement("isImporter")]
+        public bool IsImporter { get; set; }
+    }
+}
