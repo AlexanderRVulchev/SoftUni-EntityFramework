4e0ca5ae023587d274407162b1edd5c75bdf7311
Alexander Valchev <106471266+AlexanderRVulchev@users.noreply.github.com>
2023-03-12 15:06:54 +0200

    Add files via upload

---
+++ 06. Extensible Markup Language - XML/19. Export Sales With Applied Discount/DTOs/Export/SalesWithAppliedDiscountDto.cs
@@ -0,0 +1,36 @@
+﻿using System.Xml.Serialization;
+
+namespace CarDealer.DTOs.Export
+{
+    [XmlType("sale")]
+    public class SalesWithAppliedDiscountDto
+    {        
+        [XmlElement("car")]
+        public SingleCar SingleCar { get; set; }
+
+        [XmlElement("discount")]
+        public int Discount { get; set; }
+
+        [XmlElement("customer-name")]
+        public string CustomerName { get; set; }
+
+        [XmlElement("price")]
+        public decimal Price { get; set; }
+
+        [XmlElement("price-with-discount")]
+        public double PriceWithDiscount { get; set; }
+    }
+
+    [XmlType("car")]
+    public class SingleCar
+    {        
+        [XmlAttribute("make")]
+         public string Make { get; set; }
+
+        [XmlAttribute("model")]
+        public string Model { get; set; }
+
+        [XmlAttribute("traveled-distance")]
+        public long TraveledDistance { get; set; }
+    }
+}
---
+++ 06. Extensible Markup Language - XML/19. Export Sales With Applied Discount/DTOs/Import/ImportCustomersDto.cs
@@ -0,0 +1,17 @@
+﻿using System.Xml.Serialization;
+
+namespace CarDealer.DTOs.Import
+{
+    [XmlType("Customer")]
+    public class ImportCustomersDto
+    {        
+        [XmlElement("name")]
+        public string Name { get; set; }
+
+        [XmlElement("birthDate")]
+        public DateTime BirthDate { get; set; }
+
+        [XmlElement("isYoungDriver")]
+        public bool isYoungDriver { get; set; }
+    }
+}
