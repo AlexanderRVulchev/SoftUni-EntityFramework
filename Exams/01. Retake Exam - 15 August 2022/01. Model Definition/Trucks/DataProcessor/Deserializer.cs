1d1d6a5ca4e462ed53d46194c6c1dd20aa6deba1
Alexander Valchev <106471266+AlexanderRVulchev@users.noreply.github.com>
2023-05-24 18:29:35 +0300

    Add files via upload

---
+++ Exams/01. Retake Exam - 15 August 2022/01. Model Definition/Trucks/DataProcessor/Deserializer.cs
@@ -0,0 +1,34 @@
+﻿namespace Trucks.DataProcessor
+{
+    using System.ComponentModel.DataAnnotations;
+    using Data;
+
+
+    public class Deserializer
+    {
+        private const string ErrorMessage = "Invalid data!";
+
+        private const string SuccessfullyImportedDespatcher
+            = "Successfully imported despatcher - {0} with {1} trucks.";
+
+        private const string SuccessfullyImportedClient
+            = "Successfully imported client - {0} with {1} trucks.";
+
+        public static string ImportDespatcher(TrucksContext context, string xmlString)
+        {
+            throw new NotImplementedException();
+        }
+        public static string ImportClient(TrucksContext context, string jsonString)
+        {
+            throw new NotImplementedException();
+        }
+
+        private static bool IsValid(object dto)
+        {
+            var validationContext = new ValidationContext(dto);
+            var validationResult = new List<ValidationResult>();
+
+            return Validator.TryValidateObject(dto, validationContext, validationResult, true);
+        }
+    }
+}
---
+++ Exams/01. Retake Exam - 15 August 2022/01. Model Definition/Trucks/DataProcessor/Serializer.cs
@@ -0,0 +1,17 @@
+﻿namespace Trucks.DataProcessor
+{
+    using Data;
+
+    public class Serializer
+    {
+        public static string ExportDespatchersWithTheirTrucks(TrucksContext context)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static string ExportClientsWithMostTrucks(TrucksContext context, int capacity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
