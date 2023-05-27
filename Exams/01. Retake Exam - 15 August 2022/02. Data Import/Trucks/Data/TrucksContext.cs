fb5899532982fbfe80336499185a18a10f36b409
Alexander Valchev <106471266+AlexanderRVulchev@users.noreply.github.com>
2023-05-27 19:12:05 +0300

    Add files via upload

---
+++ Exams/01. Retake Exam - 15 August 2022/02. Data Import/Trucks/Data/TrucksContext.cs
@@ -0,0 +1,39 @@
+﻿namespace Trucks.Data
+{
+    using Microsoft.EntityFrameworkCore;
+    using Trucks.Data.Models;
+
+    public class TrucksContext : DbContext
+    {
+        public TrucksContext()
+        { 
+        }
+
+        public TrucksContext(DbContextOptions options)
+            : base(options) 
+        { 
+        }
+
+        public virtual DbSet<Client> Clients { get; set; }
+        public virtual DbSet<ClientTruck> ClientsTrucks { get; set; }
+        public virtual DbSet<Despatcher> Despatchers { get; set; }
+        public virtual DbSet<Truck> Trucks { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder
+                    .UseSqlServer(Configuration.ConnectionString);
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<ClientTruck>(entity =>
+            {
+                entity.HasKey(ct => new { ct.TruckId, ct.ClientId });
+            });
+        }
+    }
+}
