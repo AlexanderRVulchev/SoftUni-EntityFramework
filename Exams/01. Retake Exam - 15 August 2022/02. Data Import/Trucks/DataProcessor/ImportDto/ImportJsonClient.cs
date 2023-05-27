9650bb2fbbdc5530590afea87714666c57b31f9d
Alexander Valchev <106471266+AlexanderRVulchev@users.noreply.github.com>
2023-05-27 19:12:37 +0300

    Add files via upload

---
+++ Exams/01. Retake Exam - 15 August 2022/02. Data Import/Trucks/DataProcessor/ImportDto/ImportJsonClient.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Trucks.Data.Models;
+
+namespace Trucks.DataProcessor.ImportDto
+{
+    public class ImportJsonClient
+    {
+        [Required, MinLength(3), MaxLength(40)]
+        public string Name { get; set; }
+
+        [Required, MinLength(2), MaxLength(40)]
+        public string Nationality { get; set; }
+
+        [Required]
+        public string Type { get; set; }
+
+        public int[] Trucks { get; set; }
+    }
+}
