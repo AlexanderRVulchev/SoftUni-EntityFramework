2ba7962ff78e44f7b6ee817bad4066e00f27ac12
Alexander Valchev <106471266+AlexanderRVulchev@users.noreply.github.com>
2023-06-01 21:25:02 +0300

    Add files via upload

---
+++ Exams/01. Retake Exam - 15 August 2022/03. Data Export/Trucks/DataProcessor/Deserializer.cs
@@ -0,0 +1,139 @@
+﻿namespace Trucks.DataProcessor
+{
+    using System.ComponentModel.DataAnnotations;
+    using System.Text;
+    using System.Xml.Serialization;
+    using Data;
+    using Newtonsoft.Json;
+    using Trucks.Data.Models;
+    using Trucks.Data.Models.Enums;
+    using Trucks.DataProcessor.ImportDto;
+
+    public class Deserializer
+    {
+        private const string ErrorMessage = "Invalid data!";
+
+        private const string SuccessfullyImportedDespatcher
+            = "Successfully imported despatcher - {0} with {1} trucks.";
+
+        private const string SuccessfullyImportedClient
+            = "Successfully imported client - {0} with {1} trucks.";
+
+        public static string ImportDespatcher(TrucksContext context, string xmlString)
+        {            
+            ImportXmlDespatcher[] despatchersDtos = Deserialize<ImportXmlDespatcher[]>(xmlString, "Despatchers");
+            StringBuilder sb = new StringBuilder();
+            List<Despatcher> despatchers = new List<Despatcher>();
+
+            foreach (ImportXmlDespatcher dto in despatchersDtos)
+            {
+                if (!IsValid(dto) || string.IsNullOrEmpty(dto.Position))
+                {
+                    sb.AppendLine(ErrorMessage);
+                    continue;
+                }
+
+                Despatcher despatcher = new Despatcher()
+                {
+                    Name = dto.Name,
+                    Position = dto.Position
+                };
+
+                List<Truck> trucks = new List<Truck>();
+                foreach (var truckDto in dto.Truck)
+                {
+                    if (!IsValid(truckDto))
+                    {
+                        sb.AppendLine(ErrorMessage);
+                        continue;
+                    }
+
+                    Truck truck = new Truck()
+                    {
+                        CargoCapacity = truckDto.CargoCapacity,
+                        CategoryType = (CategoryType)truckDto.CategoryType,
+                        TankCapacity = truckDto.TankCapacity,
+                        MakeType = (MakeType)truckDto.MakeType,
+                        RegistrationNumber = truckDto.RegistrationNumber,
+                        VinNumber = truckDto.VinNumber,                        
+                        Despatcher = despatcher
+                    };
+                    trucks.Add(truck);
+                }
+                despatcher.Trucks = trucks;                
+
+                despatcher.Trucks = trucks;
+                despatchers.Add(despatcher);
+                sb.AppendLine(String.Format(SuccessfullyImportedDespatcher, despatcher.Name, despatcher.Trucks.Count));                
+            }
+            context.AddRange(despatchers);
+            context.SaveChanges();
+            return sb.ToString().TrimEnd();
+        }
+
+        public static string ImportClient(TrucksContext context, string jsonString)
+        {
+            ImportJsonClient[] clientDtos = JsonConvert.DeserializeObject<ImportJsonClient[]>(jsonString);
+            StringBuilder sb = new StringBuilder();
+
+            List<Client> clients = new List<Client>();
+            List<ClientTruck> clientsTrucks = new List<ClientTruck>();
+
+            foreach (var clientDto in clientDtos)
+            {
+                if (!IsValid(clientDto) || clientDto.Type == "usual")
+                {
+                    sb.AppendLine(ErrorMessage);
+                    continue;
+                }
+
+                Client client = new Client()
+                {
+                    Name = clientDto.Name,
+                    Nationality = clientDto.Nationality,
+                    Type = clientDto.Type
+                };
+                                                
+
+                foreach (int truckId in clientDto.Trucks.Distinct())
+                {
+                    Truck truck = context.Trucks.FirstOrDefault(t => t.Id == truckId);
+                    if (truck == null)
+                    {
+                        sb.AppendLine(ErrorMessage);
+                        continue;
+                    };
+                    client.ClientsTrucks.Add(new ClientTruck()
+                    {
+                        Client = client,
+                        Truck = truck
+                    });
+                }                
+                clients.Add(client);
+                sb.AppendLine(string.Format(SuccessfullyImportedClient, client.Name, client.ClientsTrucks.Count));
+            }
+            context.AddRange(clients);
+            context.SaveChanges();
+            return sb.ToString().TrimEnd();
+        }
+
+        private static T Deserialize<T>(string inputXml, string rootName)
+        {
+            XmlRootAttribute root = new XmlRootAttribute(rootName);
+            XmlSerializer serializer = new XmlSerializer(typeof(T), root);
+
+            using StringReader reader = new StringReader(inputXml);
+
+            T dtos = (T)serializer.Deserialize(reader);
+            return dtos;
+        }
+
+        private static bool IsValid(object dto)
+        {
+            var validationContext = new ValidationContext(dto);
+            var validationResult = new List<ValidationResult>();
+
+            return Validator.TryValidateObject(dto, validationContext, validationResult, true);
+        }
+    }
+}
---
+++ Exams/01. Retake Exam - 15 August 2022/03. Data Export/Trucks/DataProcessor/Serializer.cs
@@ -0,0 +1,85 @@
+﻿namespace Trucks.DataProcessor
+{
+    using Data;
+    using Microsoft.EntityFrameworkCore;
+    using Newtonsoft.Json;
+    using System.Text;
+    using System.Xml.Serialization;
+    using Trucks.Data.Models.Enums;
+    using Trucks.DataProcessor.ExportDto;
+
+    public class Serializer
+    {
+        public static string ExportDespatchersWithTheirTrucks(TrucksContext context)
+        {
+            ExportDespatcherDto[] despatcherDtos = context
+                .Despatchers
+                .Where(d => d.Trucks.Any())
+                .ToArray()
+                .Select(d => new ExportDespatcherDto()
+                {
+                    TrucksCount = d.Trucks.Count(),
+                    DespatcherName = d.Name,
+                    Trucks = d.Trucks.Select(t => new ExportTruckDto
+                    {
+                        Make = t.MakeType.ToString(),
+                        RegistrationNumber = t.RegistrationNumber
+                    })
+                    .OrderBy(t => t.RegistrationNumber)
+                    .ToArray()
+                })
+                .OrderByDescending(d => d.Trucks.Count())
+                .ThenBy(d => d.DespatcherName)
+                .ToArray();
+
+            return Serialize<ExportDespatcherDto[]>(despatcherDtos, "Despatchers");
+        }
+
+        public static string ExportClientsWithMostTrucks(TrucksContext context, int capacity)
+        {
+            var clients = context
+                .Clients
+                .Where(c => c.ClientsTrucks.Any(ct => ct.Truck.TankCapacity >= capacity))                
+                .ToArray()
+                .Select(c => new
+                {
+                    Name = c.Name,
+                    Trucks = c.ClientsTrucks
+                    .Where(ct => ct.Truck.TankCapacity >= capacity)
+                    .Select(ct => new
+                    {
+                        TruckRegistrationNumber = ct.Truck.RegistrationNumber,
+                        VinNumber = ct.Truck.VinNumber,
+                        TankCapacity = ct.Truck.TankCapacity,
+                        CargoCapacity = ct.Truck.CargoCapacity,
+                        CategoryType = ct.Truck.CategoryType.ToString(),
+                        MakeType = ct.Truck.MakeType.ToString()
+                    })
+                    .OrderBy(t => t.MakeType)
+                    .ThenByDescending(t => t.CargoCapacity)
+                    .ToArray()
+                })
+                .OrderByDescending(c => c.Trucks.Count())
+                .ThenBy(c => c.Name)
+                .Take(10)
+                .ToArray();
+
+            return JsonConvert.SerializeObject(clients, Formatting.Indented);
+        }
+
+        private static string Serialize<T>(T dataTransferObjects, string xmlRootAttributeName)
+        {
+            XmlSerializer serializer = new XmlSerializer(typeof(T), new XmlRootAttribute(xmlRootAttributeName));
+
+            StringBuilder sb = new StringBuilder();
+            using var write = new StringWriter(sb);
+
+            XmlSerializerNamespaces xmlNamespaces = new XmlSerializerNamespaces();
+            xmlNamespaces.Add(string.Empty, string.Empty);
+
+            serializer.Serialize(write, dataTransferObjects, xmlNamespaces);
+
+            return sb.ToString();
+        }
+    }
+}
