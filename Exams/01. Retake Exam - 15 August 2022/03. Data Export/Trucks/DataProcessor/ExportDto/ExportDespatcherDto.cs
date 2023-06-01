2908d4ce17649af4f429e1d090ba74842b9f70f6
Alexander Valchev <106471266+AlexanderRVulchev@users.noreply.github.com>
2023-06-01 21:25:27 +0300

    Add files via upload

---
+++ Exams/01. Retake Exam - 15 August 2022/03. Data Export/Trucks/DataProcessor/ExportDto/ExportDespatcherDto.cs
@@ -0,0 +1,27 @@
+﻿using System.Xml.Serialization;
+
+namespace Trucks.DataProcessor.ExportDto
+{
+    [XmlType("Despatcher")]
+    public class ExportDespatcherDto
+    {
+        [XmlAttribute("TrucksCount")]
+        public int TrucksCount { get; set; }
+
+        [XmlElement("DespatcherName")]
+        public string DespatcherName { get; set; }
+
+        [XmlArray("Trucks")]
+        public ExportTruckDto[] Trucks { get; set; }
+    }
+
+    [XmlType("Truck")]
+    public class ExportTruckDto
+    {
+        [XmlElement("RegistrationNumber")]
+        public string RegistrationNumber { get; set; }
+        
+        [XmlElement("Make")]
+        public string Make { get; set; }
+    }
+}
---
+++ Exams/01. Retake Exam - 15 August 2022/03. Data Export/Trucks/DataProcessor/ImportDto/ImportXmlDespatcher.cs
@@ -0,0 +1,54 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Xml.Serialization;
+using Trucks.Data.Models.Enums;
+
+namespace Trucks.DataProcessor.ImportDto
+{
+    [XmlType("Despatcher")]
+    public class ImportXmlDespatcher
+    {
+        [Required]
+        [MinLength(2)]
+        [MaxLength(40)]
+        [XmlElement("Name")]
+        public string Name { get; set; }
+
+        [XmlElement("Position")]
+        public string Position { get; set; }
+
+        [XmlArray("Trucks")]
+        public TruckDto[] Truck { get; set; }
+    }
+
+    [XmlType("Truck")]
+    public class TruckDto
+    {
+        [Required]
+        [RegularExpression(@"[A-Z]{2}\d{4}[A-Z]{2}$")]
+        [XmlElement("RegistrationNumber")]
+        public string RegistrationNumber { get; set; }
+
+        [Required]
+        [StringLength(17)]
+        [XmlElement("VinNumber")]
+        public string VinNumber { get; set; }
+
+        [Range(950, 1420)]
+        [XmlElement("TankCapacity")]
+        public int TankCapacity { get; set; }
+
+        [Range(5000, 29000)]
+        [XmlElement("CargoCapacity")]
+        public int CargoCapacity { get; set; }
+
+        [Required]
+        [Range(0, 3)]
+        [XmlElement("CategoryType")]
+        public int CategoryType { get; set; }
+
+        [Required]
+        [Range(0, 4)]
+        [XmlElement("MakeType")]
+        public int MakeType { get; set; }
+    }
+}
