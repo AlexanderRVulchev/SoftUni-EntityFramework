1de150dcf1592c9507c1812c13c60567eac36f66
Alexander Valchev <106471266+AlexanderRVulchev@users.noreply.github.com>
2023-06-01 21:24:23 +0300

    Add files via upload

---
+++ Exams/01. Retake Exam - 15 August 2022/03. Data Export/Trucks/Data/Models/Client.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Trucks.Data.Models
+{
+    public class Client
+    {
+        public Client()
+        {
+            ClientsTrucks = new HashSet<ClientTruck>();
+        }
+
+        [Key]
+        public int Id { get; set; }
+        
+        [Required]
+        public string Name { get; set; }
+        
+        [Required, MaxLength(40)]
+        public string Nationality { get; set; }
+        
+        [Required]
+        public string Type { get; set; }
+        
+        public virtual ICollection<ClientTruck> ClientsTrucks { get; set; }
+    }
+}
---
+++ Exams/01. Retake Exam - 15 August 2022/03. Data Export/Trucks/Data/Models/Despatcher.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Trucks.Data.Models
+{
+    public class Despatcher
+    {
+        public Despatcher()
+        {
+            Trucks = new HashSet<Truck>();
+        }
+
+        [Key]
+        public int Id { get; set; }
+        
+        [Required, MaxLength(40)]
+        public string Name { get; set; }
+        
+        public string Position { get; set; }
+        
+        public virtual ICollection<Truck> Trucks { get; set; }
+    }
+}
