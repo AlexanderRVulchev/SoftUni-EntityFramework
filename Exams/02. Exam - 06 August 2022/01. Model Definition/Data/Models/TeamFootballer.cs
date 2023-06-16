3e9405f7ba681e8e9c45196c290741b34f292323
Alexander Valchev <106471266+AlexanderRVulchev@users.noreply.github.com>
2023-06-16 19:14:15 +0300

    Add files via upload

---
+++ Exams/02. Exam - 06 August 2022/01. Model Definition/Data/Models/TeamFootballer.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Footballers.Data.Models
+{
+    public class TeamFootballer
+    {
+        [Required, ForeignKey(nameof(Team))]
+        public int TeamId { get; set; }
+        public Team Team { get; set; }
+
+        [Required, ForeignKey(nameof(Footballer))]
+        public int FootballerId { get; set; }
+        public Footballer Footballer { get; set; }
+    }
+}
