e8642168bb94ecce23728842246e41b5d998d6f8
Alexander Valchev <106471266+AlexanderRVulchev@users.noreply.github.com>
2023-06-16 19:13:30 +0300

    Add files via upload

---
+++ Exams/02. Exam - 06 August 2022/01. Model Definition/DataProcessor/Deserializer.cs
@@ -0,0 +1,34 @@
+﻿namespace Footballers.DataProcessor
+{
+    using Footballers.Data;
+    using System.ComponentModel.DataAnnotations;
+
+    public class Deserializer
+    {
+        private const string ErrorMessage = "Invalid data!";
+
+        private const string SuccessfullyImportedCoach
+            = "Successfully imported coach - {0} with {1} footballers.";
+
+        private const string SuccessfullyImportedTeam
+            = "Successfully imported team - {0} with {1} footballers.";
+
+        public static string ImportCoaches(FootballersContext context, string xmlString)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static string ImportTeams(FootballersContext context, string jsonString)
+        {
+            throw new NotImplementedException();
+        }
+
+        private static bool IsValid(object dto)
+        {
+            var validationContext = new ValidationContext(dto);
+            var validationResult = new List<ValidationResult>();
+
+            return Validator.TryValidateObject(dto, validationContext, validationResult, true);
+        }
+    }
+}
---
+++ Exams/02. Exam - 06 August 2022/01. Model Definition/DataProcessor/Serializer.cs
@@ -0,0 +1,17 @@
+﻿namespace Footballers.DataProcessor
+{
+    using Data;
+
+    public class Serializer
+    {
+        public static string ExportCoachesWithTheirFootballers(FootballersContext context)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static string ExportTeamsWithMostFootballers(FootballersContext context, DateTime date)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
