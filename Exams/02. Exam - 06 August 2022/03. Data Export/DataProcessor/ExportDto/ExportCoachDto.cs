8584f2bc08ee2d7039b0f9878d5fb25ed774ba00
Alexander Valchev <106471266+AlexanderRVulchev@users.noreply.github.com>
2023-06-23 22:26:23 +0300

    Add files via upload

---
+++ Exams/02. Exam - 06 August 2022/03. Data Export/DataProcessor/ExportDto/ExportCoachDto.cs
@@ -0,0 +1,27 @@
+﻿using System.Xml.Serialization;
+
+namespace Footballers.DataProcessor.ExportDto
+{
+    [XmlType("Coach")]
+    public class ExportCoachDto
+    {
+        [XmlAttribute("FootballersCount")]
+        public int FootballersCount { get; set; }
+
+        [XmlElement("CoachName")]
+        public string CoachName { get; set; }
+
+        [XmlArray("Footballers")]
+        public ExportFootballerDto[] Footballers { get; set; }
+    }
+
+    [XmlType("Footballer")]
+    public class ExportFootballerDto
+    {
+        [XmlElement("Name")]
+        public string Name { get; set; }
+
+        [XmlElement("Position")]
+        public string Position { get; set; }
+    }
+}
