e0519332d382afae14a780ff9aa1a320192be556
Alexander Valchev <106471266+AlexanderRVulchev@users.noreply.github.com>
2023-06-23 22:26:04 +0300

    Add files via upload

---
+++ Exams/02. Exam - 06 August 2022/03. Data Export/DataProcessor/Deserializer.cs
@@ -0,0 +1,152 @@
+﻿namespace Footballers.DataProcessor
+{
+    using Footballers.Data;
+    using Footballers.Data.Models;
+    using Footballers.Data.Models.Enums;
+    using Footballers.DataProcessor.ImportDto;
+    using Newtonsoft.Json;
+    using System.ComponentModel.DataAnnotations;
+    using System.Globalization;
+    using System.Net;
+    using System.Reflection.Metadata.Ecma335;
+    using System.Text;
+    using System.Xml.Serialization;
+
+    public class Deserializer
+    {
+        private const string ErrorMessage = "Invalid data!";
+
+        private const string SuccessfullyImportedCoach
+            = "Successfully imported coach - {0} with {1} footballers.";
+
+        private const string SuccessfullyImportedTeam
+            = "Successfully imported team - {0} with {1} footballers.";
+
+        public static string ImportCoaches(FootballersContext context, string xmlString)
+        {
+            ImportCoachDto[] coachDtos = Deserialize<ImportCoachDto[]>(xmlString, "Coaches");
+
+            List<Coach> coaches = new List<Coach>();
+            StringBuilder sb = new StringBuilder();
+
+            foreach (var coachDto in coachDtos)
+            {
+                if (!IsValid(coachDto) || string.IsNullOrEmpty(coachDto.Nationality))
+                {
+                    sb.AppendLine(ErrorMessage);
+                    continue;
+                }
+
+                Coach coach = new Coach()
+                {
+                    Name = coachDto.Name,
+                    Nationality = coachDto.Nationality
+                };
+
+                List<Footballer> footballers = new List<Footballer>();
+                foreach (var footballerDto in coachDto.Footballers)
+                {
+                    if (!IsValid(footballerDto))
+                    {
+                        sb.AppendLine(ErrorMessage);
+                        continue;
+                    }
+
+                    DateTime validStartDate;
+                    bool isStartDateValid = DateTime.TryParseExact(footballerDto.ContractStartDate, "dd/MM/yyyy", CultureInfo.InvariantCulture, DateTimeStyles.None, out validStartDate);
+
+                    DateTime validEndDate;
+                    bool isEndDateValid = DateTime.TryParseExact(footballerDto.ContractEndDate, "dd/MM/yyyy", CultureInfo.InvariantCulture, DateTimeStyles.None, out validEndDate);
+
+                    if (!isStartDateValid || !isEndDateValid || validEndDate < validStartDate)
+                    {
+                        sb.AppendLine(ErrorMessage);
+                        continue;
+                    }
+
+                    Footballer footballer = new Footballer()
+                    {
+                        Name = footballerDto.Name,
+                        BestSkillType = (BestSkillType)footballerDto.BestSkillType,
+                        PositionType = (PositionType)footballerDto.PositionType,
+                        ContractStartDate = validStartDate,
+                        ContractEndDate = validEndDate,
+                        Coach = coach
+                    };
+
+                    footballers.Add(footballer);
+                }
+                coach.Footballers = footballers.ToArray();
+                coaches.Add(coach);
+                sb.AppendLine(string.Format(SuccessfullyImportedCoach, coach.Name, footballers.Count));
+            }
+            context.Coaches.AddRange(coaches);
+            context.SaveChanges();
+            return sb.ToString().TrimEnd();
+        }
+
+        public static string ImportTeams(FootballersContext context, string jsonString)
+        {
+            ImportTeamDto[] teamDtos = JsonConvert.DeserializeObject<ImportTeamDto[]>(jsonString);
+            StringBuilder sb = new StringBuilder();
+            List<Team> teams = new List<Team>();
+
+            foreach (var teamDto in teamDtos)
+            {
+                if (!IsValid(teamDto))
+                {
+                    sb.AppendLine(ErrorMessage);
+                    continue;
+                }
+
+                Team team = new Team()
+                {
+                    Name = teamDto.Name,
+                    Nationality = teamDto.Nationality,
+                    Trophies = teamDto.Trophies
+                };
+
+                foreach (int footballerId in teamDto.Footballers.Distinct())
+                {
+                    Footballer footballer = context.Footballers.Find(footballerId);
+
+                    if (footballer == null)
+                    {
+                        sb.AppendLine(ErrorMessage);
+                        continue;
+                    }
+
+                    team.TeamsFootballers.Add(new TeamFootballer()
+                    {
+                        Footballer = footballer,
+                        Team = team
+                    });
+                }
+                teams.Add(team);
+                sb.AppendLine(string.Format(SuccessfullyImportedTeam, team.Name, team.TeamsFootballers.Count));
+            }
+            context.AddRange(teams);
+            context.SaveChanges();
+            return sb.ToString().TrimEnd();
+        }
+
+        private static T Deserialize<T>(string inputXml, string rootName)
+        {
+            XmlRootAttribute root = new XmlRootAttribute(rootName);
+            XmlSerializer serializer = new XmlSerializer(typeof(T), root);
+
+            using StringReader reader = new StringReader(inputXml);
+
+            T dtos = (T)serializer.Deserialize(reader);
+            return dtos;
+        }
+
+        private static bool IsValid(object dto)
+        {
+            var validationContext = new ValidationContext(dto);
+            var validationResult = new List<ValidationResult>();
+
+            return Validator.TryValidateObject(dto, validationContext, validationResult, true);
+        }
+    }
+}
---
+++ Exams/02. Exam - 06 August 2022/03. Data Export/DataProcessor/Serializer.cs
@@ -0,0 +1,85 @@
+﻿namespace Footballers.DataProcessor
+{
+    using Data;
+    using Footballers.Data.Models.Enums;
+    using Footballers.DataProcessor.ExportDto;
+    using Newtonsoft.Json;
+    using System.Globalization;
+    using System.Text;
+    using System.Xml.Serialization;
+
+    public class Serializer
+    {
+        public static string ExportCoachesWithTheirFootballers(FootballersContext context)
+        {
+            ExportCoachDto[] coachDtos = context
+                .Coaches
+                .Where(c => c.Footballers.Any())
+                .ToArray()
+                .Select(c => new ExportCoachDto()
+                {
+                    CoachName = c.Name,
+                    FootballersCount = c.Footballers.Count,
+                    Footballers = c.Footballers.Select(f => new ExportFootballerDto()
+                        {
+                            Name = f.Name,
+                            Position = f.PositionType.ToString()
+                        })
+                        .OrderBy(f => f.Name)
+                        .ToArray()
+                })
+                .OrderByDescending(c => c.FootballersCount)
+                .ThenBy(c => c.CoachName)
+                .ToArray();
+
+            return Serialize<ExportCoachDto[]>(coachDtos, "Coaches");
+        }
+
+        public static string ExportTeamsWithMostFootballers(FootballersContext context, DateTime date)
+        {
+            var exportTeams = context
+                .Teams
+                .Where(t => t.TeamsFootballers.Any(tf => tf.Footballer.ContractStartDate >= date))
+                .ToArray()
+                .Select(t => new
+                {
+                    Name = t.Name,
+                    Footballers = t.TeamsFootballers
+                        .Where(tf => tf.Footballer.ContractStartDate >= date)
+                        .OrderByDescending(tf => tf.Footballer.ContractEndDate)
+                        .ThenBy(tf => tf.Footballer.Name)
+                        .ToArray()
+                        .Select(tf => new
+                        {
+                            FootballerName = tf.Footballer.Name,
+                            ContractStartDate = tf.Footballer.ContractStartDate.ToString("d", CultureInfo.InvariantCulture),
+                            ContractEndDate = tf.Footballer.ContractEndDate.ToString("d", CultureInfo.InvariantCulture),
+                            BestSkillType = tf.Footballer.BestSkillType.ToString(),
+                            PositionType = tf.Footballer.PositionType.ToString()
+                        })                        
+                        .ToArray()
+                })
+                .OrderByDescending(t => t.Footballers.Count())
+                .ThenBy(t => t.Name)
+                .Take(5)
+                .ToArray();
+
+            return JsonConvert.SerializeObject(exportTeams, Formatting.Indented);
+        }
+
+        private static string Serialize<T>(T dataTransferObjects, string xmlRootAttributeName)
+        {
+            XmlSerializer serializer = new XmlSerializer(typeof(T), new XmlRootAttribute(xmlRootAttributeName));
+
+            StringBuilder sb = new StringBuilder();
+            using var write = new StringWriter(sb);
+
+            XmlSerializerNamespaces xmlNamespaces = new XmlSerializerNamespaces();
+            xmlNamespaces.Add(string.Empty, string.Empty);
+
+            serializer.Serialize(write, dataTransferObjects, xmlNamespaces);
+
+            return sb.ToString();
+        }
+    }
+}
