42279d90a10adb24324e341ca88237ede351434e
Alexander Valchev <106471266+AlexanderRVulchev@users.noreply.github.com>
2023-06-23 22:26:39 +0300

    Add files via upload

---
+++ Exams/02. Exam - 06 August 2022/03. Data Export/DataProcessor/ImportDto/ImportCoachDto.cs
@@ -0,0 +1,48 @@
+﻿using Footballers.Data.Models.Enums;
+using Footballers.Data.Models;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Xml.Serialization;
+
+namespace Footballers.DataProcessor.ImportDto
+{
+    [XmlType("Coach")]
+    public class ImportCoachDto
+    {
+        [XmlElement("Name")]
+        [Required]
+        [MinLength(2)]
+        [MaxLength(40)]
+        public string Name { get; set; }
+
+        [XmlElement("Nationality")]
+        [Required]
+        public string Nationality { get; set; }
+
+        public ImportFootballerDto[] Footballers { get; set; }
+    }
+
+    [XmlType("Footballer")]
+    public class ImportFootballerDto
+    {
+        [XmlElement("Name")]
+        [Required]
+        [MinLength(2)]
+        [MaxLength(40)]
+        public string Name { get; set; }
+
+        [XmlElement("ContractStartDate")]
+        [Required]
+        public string ContractStartDate { get; set; }
+
+        [XmlElement("ContractEndDate")]
+        [Required]
+        public string ContractEndDate { get; set; }
+
+        [Required]
+        public int PositionType { get; set; }
+
+        [Required]
+        public int BestSkillType { get; set; }
+    }
+}
---
+++ Exams/02. Exam - 06 August 2022/03. Data Export/DataProcessor/ImportDto/ImportTeamDto.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Footballers.DataProcessor.ImportDto
+{
+    public class ImportTeamDto
+    {
+        [Required]
+        [MinLength(3)]
+        [MaxLength(40)]
+        [RegularExpression(@"[a-zA-z.\-\d\s]+$")]
+        public string Name { get; set; }
+
+        [Required]
+        [MinLength(2)]
+        [MaxLength(40)]
+        public string Nationality { get; set; }
+
+        [Required]
+        [Range(1, int.MaxValue)]
+        public int Trophies { get; set; }
+
+        public int[] Footballers { get; set; }
+    }
+}
