68d0cbce676b1ba94d4deb2ecddc0d0c345acc7f
Alexander Valchev <106471266+AlexanderRVulchev@users.noreply.github.com>
2023-07-06 21:02:03 +0300

    Add files via upload

---
+++ Exams/03. Retake Exam - 16 Dec 2021/01. Model Definition/Artillery/Data/Models/Shell.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Artillery.Data.Models
+{
+    public class Shell
+    {
+        public Shell()
+        {
+            Guns = new HashSet<Gun>();
+        }
+
+        [Key]
+        public int Id { get; set; }
+        
+        [Required]
+        public double ShellWeight { get; set; }
+        
+        [Required]
+        public string Caliber { get; set; }
+        
+        public ICollection<Gun> Guns { get; set; }
+    }
+}
