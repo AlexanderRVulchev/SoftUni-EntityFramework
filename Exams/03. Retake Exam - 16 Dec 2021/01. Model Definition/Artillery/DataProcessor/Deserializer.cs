3602f65853c43e91628dd2ba0c75cb9d0ea1b5ed
Alexander Valchev <106471266+AlexanderRVulchev@users.noreply.github.com>
2023-07-06 21:02:20 +0300

    Add files via upload

---
+++ Exams/03. Retake Exam - 16 Dec 2021/01. Model Definition/Artillery/DataProcessor/Deserializer.cs
@@ -0,0 +1,47 @@
+﻿namespace Artillery.DataProcessor
+{
+    using Artillery.Data;
+    using System.ComponentModel.DataAnnotations;
+
+    public class Deserializer
+    {
+        private const string ErrorMessage =
+            "Invalid data.";
+        private const string SuccessfulImportCountry =
+            "Successfully import {0} with {1} army personnel.";
+        private const string SuccessfulImportManufacturer =
+            "Successfully import manufacturer {0} founded in {1}.";
+        private const string SuccessfulImportShell =
+            "Successfully import shell caliber #{0} weight {1} kg.";
+        private const string SuccessfulImportGun =
+            "Successfully import gun {0} with a total weight of {1} kg. and barrel length of {2} m.";
+
+        public static string ImportCountries(ArtilleryContext context, string xmlString)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static string ImportManufacturers(ArtilleryContext context, string xmlString)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static string ImportShells(ArtilleryContext context, string xmlString)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static string ImportGuns(ArtilleryContext context, string jsonString)
+        {
+            throw new NotImplementedException();
+        }
+        private static bool IsValid(object obj)
+        {
+            var validator = new ValidationContext(obj);
+            var validationRes = new List<ValidationResult>();
+
+            var result = Validator.TryValidateObject(obj, validator, validationRes, true);
+            return result;
+        }
+    }
+}
