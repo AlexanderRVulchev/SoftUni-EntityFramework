deb7a92eb609cd65df97a07885acecf0baf28422
Alexander Valchev <106471266+AlexanderRVulchev@users.noreply.github.com>
2023-07-10 09:05:03 +0300

    Add files via upload

---
+++ Exams/03. Retake Exam - 16 Dec 2021/02. Data Import/Artillery/Models/Country.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Artillery.Data.Models
+{
+    public class Country
+    {
+        public Country()
+        {
+            CountriesGuns = new HashSet<CountryGun>();
+        }
+
+        [Key]
+        public int Id { get; set; }
+        
+        [Required]
+        public string CountryName { get; set; }
+        
+        [Required]
+        public int ArmySize { get; set; }
+        
+        public ICollection<CountryGun> CountriesGuns { get; set; }
+    }
+}
---
+++ Exams/03. Retake Exam - 16 Dec 2021/02. Data Import/Artillery/Models/Manufacturer.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Artillery.Data.Models
+{
+    public class Manufacturer
+    {
+        public Manufacturer()
+        {
+            Guns = new HashSet<Gun>();
+        }
+        
+        [Key]
+        public int Id { get; set; }
+        
+        [Required]
+        public string ManufacturerName { get; set; }
+        
+        [Required]
+        public string Founded { get; set; }
+        
+        public ICollection<Gun> Guns { get; set; }
+    }
+}
