072e4c6f6e95a547ddfa18557a2d1e025610858e
Alexander Valchev <106471266+AlexanderRVulchev@users.noreply.github.com>
2023-07-10 09:03:35 +0300

    Add files via upload

---
+++ Exams/03. Retake Exam - 16 Dec 2021/02. Data Import/Artillery/Data/Models/CountryGun.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Artillery.Data.Models
+{
+    public class CountryGun
+    {        
+        [Required, ForeignKey(nameof(Country))]
+        public int CountryId { get; set; }
+        public Country Country { get; set; }
+        
+        [Required, ForeignKey(nameof(Gun))]
+        public int GunId { get; set; }
+        public Gun Gun { get; set; }
+    }
+}
