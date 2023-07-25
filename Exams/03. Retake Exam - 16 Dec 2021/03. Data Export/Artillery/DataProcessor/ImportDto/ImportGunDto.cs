683dbe39520a3dd948e0c32d0166f4267aae0549
Alexander Valchev <106471266+AlexanderRVulchev@users.noreply.github.com>
2023-07-25 19:58:14 +0300

    Add files via upload

---
+++ Exams/03. Retake Exam - 16 Dec 2021/03. Data Export/Artillery/DataProcessor/ImportDto/ImportGunDto.cs
@@ -0,0 +1,31 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Artillery.DataProcessor.ImportDto
+{
+    public class ImportGunDto
+    {        
+        public int ManufacturerId { get; set; }
+                
+        [Range(100, 1_350_000)]
+        public int GunWeight { get; set; }
+                
+        [Range(2.00, 35.00)]
+        public double BarrelLength { get; set; }
+        
+        public int? NumberBuild { get; set; }
+        
+        [Range(1, 100_000)]
+        public int Range { get; set; }
+        
+        public string GunType { get; set; }
+        
+        public int ShellId { get; set; }
+        
+        public ImportCountryIdsDto[] Countries { get; set; }
+    }
+
+    public class ImportCountryIdsDto
+    {
+        public int Id { get; set; }
+    }
+}
---
+++ Exams/03. Retake Exam - 16 Dec 2021/03. Data Export/Artillery/DataProcessor/ImportDto/ImportShellDto.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Xml.Serialization;
+
+namespace Artillery.DataProcessor.ImportDto
+{
+    [XmlType("Shell")]
+    public class ImportShellDto
+    {
+        [XmlElement("ShellWeight")]        
+        [Range(2, 1680)]
+        public double ShellWeight { get; set; }
+
+        [XmlElement("Caliber")]         
+        [MinLength(4)]
+        [MaxLength(30)]
+        public string Caliber { get; set; }
+    }
+}
