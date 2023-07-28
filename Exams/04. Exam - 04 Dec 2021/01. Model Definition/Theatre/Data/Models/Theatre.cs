b8990189fd56dc9e7ffdaff2aacd3eccd951ade9
Alexander Valchev <106471266+AlexanderRVulchev@users.noreply.github.com>
2023-07-28 22:24:08 +0300

    Add files via upload

---
+++ Exams/04. Exam - 04 Dec 2021/01. Model Definition/Theatre/Data/Models/Theatre.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Theatre.Data.Models
+{
+    public class Theatre
+    {
+        public Theatre()
+        {
+            Tickets = new HashSet<Ticket>();
+        }
+        
+        [Key]
+        public int Id { get; set; }
+        
+        [Required]
+        public string Name { get; set; }
+        
+        [Required]
+        public sbyte NumberOfHalls { get; set; }
+        
+        [Required]
+        public string Director { get; set; }
+        
+        public ICollection<Ticket> Tickets { get; set; }
+    }
+}
