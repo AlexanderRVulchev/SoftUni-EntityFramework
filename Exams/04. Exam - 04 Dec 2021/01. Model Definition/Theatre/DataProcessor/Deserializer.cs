3247e5f487108a1a5952b8408b2df79cb0926a66
Alexander Valchev <106471266+AlexanderRVulchev@users.noreply.github.com>
2023-07-28 22:24:35 +0300

    Add files via upload

---
+++ Exams/04. Exam - 04 Dec 2021/01. Model Definition/Theatre/DataProcessor/Deserializer.cs
@@ -0,0 +1,48 @@
+﻿namespace Theatre.DataProcessor
+{
+    using System.ComponentModel.DataAnnotations;    
+    using System.Text;
+    using System.Xml.Serialization;
+    using Theatre.Data;      
+    
+
+    public class Deserializer
+    {
+        private const string ErrorMessage = "Invalid data!";
+
+        private const string SuccessfulImportPlay
+            = "Successfully imported {0} with genre {1} and a rating of {2}!";
+
+        private const string SuccessfulImportActor
+            = "Successfully imported actor {0} as a {1} character!";
+
+        private const string SuccessfulImportTheatre
+            = "Successfully imported theatre {0} with #{1} tickets!";
+
+
+
+        public static string ImportPlays(TheatreContext context, string xmlString)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static string ImportCasts(TheatreContext context, string xmlString)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static string ImportTtheatersTickets(TheatreContext context, string jsonString)
+        {
+            throw new NotImplementedException();
+        }
+
+        private static bool IsValid(object obj)
+        {
+            var validator = new ValidationContext(obj);
+            var validationRes = new List<ValidationResult>();
+
+            var result = Validator.TryValidateObject(obj, validator, validationRes, true);
+            return result;
+        }
+    }
+}
