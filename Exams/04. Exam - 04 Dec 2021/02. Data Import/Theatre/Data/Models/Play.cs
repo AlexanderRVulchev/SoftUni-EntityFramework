9073ee17e9e61a12ab2b8689a00ad826b0c773bb
Alexander Valchev <106471266+AlexanderRVulchev@users.noreply.github.com>
2023-07-30 09:21:07 +0300

    Add files via upload

---
+++ Exams/04. Exam - 04 Dec 2021/02. Data Import/Theatre/Data/Models/Play.cs
@@ -0,0 +1,39 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Theatre.Data.Models.Enums;
+
+namespace Theatre.Data.Models
+{
+    public class Play
+    {
+        public Play()
+        {
+            Casts = new HashSet<Cast>();
+            Tickets = new HashSet<Ticket>();
+        }
+
+        [Key]
+        public int Id { get; set; }
+        
+        [Required]
+        public string Title { get; set; }
+        
+        [Required]
+        public TimeSpan Duration { get; set; }
+        
+        [Required]
+        public float Rating { get; set; }
+        
+        [Required]
+        public Genre Genre { get; set; }
+        
+        [Required]
+        public string Description { get; set; }
+        
+        [Required]
+        public string Screenwriter { get; set; }
+        
+        public ICollection<Cast> Casts { get; set; }
+        
+        public ICollection<Ticket> Tickets { get; set; }
+    }
+}
