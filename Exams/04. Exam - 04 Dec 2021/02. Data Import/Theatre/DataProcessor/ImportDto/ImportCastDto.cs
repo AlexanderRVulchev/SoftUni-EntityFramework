5184bcb4ef297a789ab650dc0f65fbffb052573d
Alexander Valchev <106471266+AlexanderRVulchev@users.noreply.github.com>
2023-07-30 09:21:25 +0300

    Add files via upload

---
+++ Exams/04. Exam - 04 Dec 2021/02. Data Import/Theatre/DataProcessor/ImportDto/ImportCastDto.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Xml.Serialization;
+using Theatre.Data.Models;
+
+namespace Theatre.DataProcessor.ImportDto
+{
+    [XmlType("Cast")]
+    public class ImportCastDto
+    {        
+        [Required]
+        [MinLength(4)]
+        [MaxLength(30)]
+        [XmlElement("FullName")]
+        public string FullName { get; set; }
+        
+        [Required]
+        [XmlElement("IsMainCharacter")]
+        public bool IsMainCharacter { get; set; }
+        
+        [Required]
+        [RegularExpression(@"^\+44-\d{2}-\d{3}-\d{4}$")]
+        [XmlElement("PhoneNumber")]
+        public string PhoneNumber { get; set; }
+        
+        [Required]
+        [XmlElement("PlayId")]
+        public int PlayId { get; set; }
+    }
+}
---
+++ Exams/04. Exam - 04 Dec 2021/02. Data Import/Theatre/DataProcessor/Serializer.cs
@@ -0,0 +1,19 @@
+﻿namespace Theatre.DataProcessor
+{
+
+    using System;
+    using Theatre.Data;
+
+    public class Serializer
+    {
+        public static string ExportTheatres(TheatreContext context, int numbersOfHalls)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static string ExportPlays(TheatreContext context, double raiting)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
