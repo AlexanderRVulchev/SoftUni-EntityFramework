3f82ade836bbded255bf4136f735c75b890375c0
Alexander Valchev <106471266+AlexanderRVulchev@users.noreply.github.com>
2023-08-02 21:10:41 +0300

    Add files via upload

---
+++ Exams/04. Exam - 04 Dec 2021/03. Data Export/Theatre/Data/Models/Cast.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Theatre.Data.Models
+{
+    public class Cast
+    {        
+        [Key]
+        public int Id { get; set; }
+        
+        [Required]
+        public string FullName { get; set; }
+        
+        [Required]
+        public bool IsMainCharacter { get; set; }
+        
+        [Required]
+        public string PhoneNumber { get; set; }
+
+        [Required, ForeignKey(nameof(Play))]
+        public int PlayId { get; set; }
+        public Play Play { get; set; }        
+    }
+}
---
+++ Exams/04. Exam - 04 Dec 2021/03. Data Export/Theatre/Data/Models/Ticket.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Theatre.Data.Models
+{
+    public class Ticket
+    {
+        [Key]
+        public int Id { get; set; }
+        
+        [Required]
+        public decimal Price { get; set; }
+        
+        [Required]
+        public sbyte RowNumber { get; set; }
+        
+        [Required, ForeignKey(nameof(Play))]
+        public int PlayId { get; set; }
+        public Play Play { get; set; }
+        
+        [Required, ForeignKey(nameof(Theatre))]
+        public int TheatreId { get; set; }
+        public Theatre Theatre { get; set; }
+    }
+}
