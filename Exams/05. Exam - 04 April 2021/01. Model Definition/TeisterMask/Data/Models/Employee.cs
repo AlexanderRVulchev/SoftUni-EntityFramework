743e61af37bc61a0a4f1344736fefc76816fad8e
Alexander Valchev <106471266+AlexanderRVulchev@users.noreply.github.com>
2023-08-04 21:39:46 +0300

    Add files via upload

---
+++ Exams/05. Exam - 04 April 2021/01. Model Definition/TeisterMask/Data/Models/Employee.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TeisterMask.Data.Models
+{
+    public class Employee
+    {
+        public Employee()
+        {
+            EmployeesTasks = new HashSet<EmployeeTask>();
+        }
+
+        [Key]
+        public int Id { get; set; }
+        
+        [Required]
+        public string Username { get; set; }
+        
+        [Required]
+        public string Email { get; set; }
+        
+        [Required]
+        public string Phone { get; set; }
+        
+        public ICollection<EmployeeTask> EmployeesTasks { get; set; }
+    }
+}
