a7c7111838d1b8928699deab2ed77e85a6481918
Alexander Valchev <106471266+AlexanderRVulchev@users.noreply.github.com>
2023-08-04 21:40:04 +0300

    Add files via upload

---
+++ Exams/05. Exam - 04 April 2021/01. Model Definition/TeisterMask/DataProcessor/Serializer.cs
@@ -0,0 +1,17 @@
+﻿namespace TeisterMask.DataProcessor
+{
+    using Data;
+
+    public class Serializer
+    {
+        public static string ExportProjectWithTheirTasks(TeisterMaskContext context)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static string ExportMostBusiestEmployees(TeisterMaskContext context, DateTime date)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
