b255eeae8655101a5b8d245e9f50a8c2d3eff10c
Alexander Valchev <106471266+AlexanderRVulchev@users.noreply.github.com>
2023-08-07 21:33:32 +0300

    Add files via upload

---
+++ Exams/05. Exam - 04 April 2021/02. Data Import/TeisterMask/Data/Models/Project.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TeisterMask.Data.Models
+{
+    public class Project
+    {
+        public Project()
+        {
+            Tasks = new HashSet<Task>();
+        }
+
+        [Key]
+        public int Id { get; set; }
+        
+        [Required]
+        public string Name { get; set; }
+        
+        [Required]
+        public DateTime OpenDate { get; set; }
+        
+        public DateTime? DueDate { get; set; }
+        
+        public ICollection<Task> Tasks { get; set; }
+    }
+}
