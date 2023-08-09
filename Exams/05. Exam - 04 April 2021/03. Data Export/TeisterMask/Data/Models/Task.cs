3a688d72cbcc499cf324ad9d4915533d18633a29
Alexander Valchev <106471266+AlexanderRVulchev@users.noreply.github.com>
2023-08-09 21:11:12 +0300

    Add files via upload

---
+++ Exams/05. Exam - 04 April 2021/03. Data Export/TeisterMask/Data/Models/Task.cs
@@ -0,0 +1,38 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using TeisterMask.Data.Models.Enums;
+
+namespace TeisterMask.Data.Models
+{
+    public class Task
+    {
+        public Task()
+        {
+            EmployeesTasks = new HashSet<EmployeeTask>();
+        }
+
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        public DateTime OpenDate { get; set; }
+
+        [Required]
+        public DateTime DueDate { get; set; }
+
+        [Required]
+        public ExecutionType ExecutionType { get; set; }
+
+        [Required]
+        public LabelType LabelType { get; set; }
+
+        [Required, ForeignKey(nameof(Project))]
+        public int ProjectId { get; set; }
+        public Project Project { get; set; }
+
+        public ICollection<EmployeeTask> EmployeesTasks { get; set; }
+    }
+}
