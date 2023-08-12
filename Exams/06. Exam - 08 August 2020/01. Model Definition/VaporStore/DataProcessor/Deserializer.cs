35e5873acdde7d284a2d1571e069c1df6255373c
Alexander Valchev <106471266+AlexanderRVulchev@users.noreply.github.com>
2023-08-12 16:24:04 +0300

    Add files via upload

---
+++ Exams/06. Exam - 08 August 2020/01. Model Definition/VaporStore/DataProcessor/Deserializer.cs
@@ -0,0 +1,40 @@
+﻿namespace VaporStore.DataProcessor
+{
+    using System.ComponentModel.DataAnnotations;
+
+    using Data;
+
+    public static class Deserializer
+    {
+        public const string ErrorMessage = "Invalid Data";
+
+        public const string SuccessfullyImportedGame = "Added {0} ({1}) with {2} tags";
+
+        public const string SuccessfullyImportedUser = "Imported {0} with {1} cards";
+
+        public const string SuccessfullyImportedPurchase = "Imported {0} for {1}";
+
+        public static string ImportGames(VaporStoreDbContext context, string jsonString)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static string ImportUsers(VaporStoreDbContext context, string jsonString)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static string ImportPurchases(VaporStoreDbContext context, string xmlString)
+        {
+            throw new NotImplementedException();
+        }
+
+        private static bool IsValid(object dto)
+        {
+            var validationContext = new ValidationContext(dto);
+            var validationResult = new List<ValidationResult>();
+
+            return Validator.TryValidateObject(dto, validationContext, validationResult, true);
+        }
+    }
+}
