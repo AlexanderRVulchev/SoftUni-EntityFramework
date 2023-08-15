259720e8ae6c6d462614fa288a7fdba01446f868
Alexander Valchev <106471266+AlexanderRVulchev@users.noreply.github.com>
2023-08-15 21:16:05 +0300

    Add files via upload

---
+++ Exams/06. Exam - 08 August 2020/02. Data Import/VaporStore/Data/Models/Developer.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace VaporStore.Data.Models
+{
+    public class Developer
+    {
+        public Developer()
+        {
+            Games = new HashSet<Game>();
+        }
+
+        [Key]
+        public int Id { get; set; }
+        
+        [Required]
+        public string Name { get; set; }
+        
+        public ICollection<Game> Games { get; set; }
+    }
+}
---
+++ Exams/06. Exam - 08 August 2020/02. Data Import/VaporStore/Data/Models/Tag.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace VaporStore.Data.Models
+{
+    public class Tag
+    {
+        public Tag()
+        {
+            GameTags = new HashSet<GameTag>();
+        }
+        
+        [Key]
+        public int Id { get; set; }
+        
+        [Required]
+        public string Name { get; set; }
+        
+        public ICollection<GameTag> GameTags { get; set; }
+    }
+}
