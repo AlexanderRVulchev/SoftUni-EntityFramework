de9c8134ce2386ba3f7ba721fb6223e5769288a6
Alexander Valchev <106471266+AlexanderRVulchev@users.noreply.github.com>
2023-08-16 18:41:05 +0300

    Add files via upload

---
+++ Exams/06. Exam - 08 August 2020/03. Data Export/VaporStore/Data/Models/Card.cs
@@ -0,0 +1,32 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using VaporStore.Data.Models.Enums;
+
+namespace VaporStore.Data.Models
+{
+    public class Card
+    {
+        public Card()
+        {
+            Purchases = new HashSet<Purchase>();
+        }
+        
+        [Key]
+        public int Id { get; set; }
+        
+        [Required]
+        public string Number { get; set; }
+        
+        [Required]
+        public string Cvc { get; set; }
+
+        [Required]
+        public CardType Type { get; set; }
+        
+        [Required, ForeignKey(nameof(User))]
+        public int UserId { get; set; }
+        public User User { get; set; }
+                
+        public ICollection<Purchase> Purchases { get; set; }
+    }
+}
---
+++ Exams/06. Exam - 08 August 2020/03. Data Export/VaporStore/Data/Models/GameTag.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace VaporStore.Data.Models
+{
+    public class GameTag
+    {        
+        [Required, ForeignKey(nameof(Game))]
+        public int GameId { get; set; }
+        public Game Game { get; set; }
+        
+        [Required, ForeignKey(nameof(Tag))]
+        public int TagId { get; set; }
+        public Tag Tag { get; set; }
+    }
+}
