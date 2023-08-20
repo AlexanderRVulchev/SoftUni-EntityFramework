b5424f661b9dc06becba48a2b0c839c46720518f
Alexander Valchev <106471266+AlexanderRVulchev@users.noreply.github.com>
2023-08-20 19:08:33 +0300

    Add files via upload

---
+++ Exams/07. Retake Exam - 14 August 2020/01. Model Definition/SoftJail/Data/Models/Prisoner.cs
@@ -0,0 +1,40 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace SoftJail.Data.Models
+{
+    public class Prisoner
+    {
+        public Prisoner()
+        {
+            Mails = new HashSet<Mail>();
+            PrisonerOfficers = new HashSet<OfficerPrisoner>();
+        }
+
+        [Key]
+        public int Id { get; set; }
+        
+        [Required]
+        public string FullName { get; set; }
+        
+        [Required]
+        public string Nickname { get; set; }
+        
+        [Required]
+        public int Age { get; set; }
+        
+        [Required]
+        public DateTime IncarcerationDate { get; set; }
+        
+        public DateTime? ReleaseDate { get; set; }
+        
+        public decimal? Bail { get; set; }
+        
+        [ForeignKey(nameof(Cell))]
+        public int? CellId { get; set; }
+        public Cell Cell { get; set; }
+        
+        public ICollection<Mail> Mails { get; set; }        
+        public ICollection<OfficerPrisoner> PrisonerOfficers { get; set; }
+    }
+}
