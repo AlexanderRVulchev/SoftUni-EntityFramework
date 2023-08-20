bc93ed056dcc80b0a4f258e14a303a7f7a76042d
Alexander Valchev <106471266+AlexanderRVulchev@users.noreply.github.com>
2023-08-20 19:08:45 +0300

    Add files via upload

---
+++ Exams/07. Retake Exam - 14 August 2020/01. Model Definition/SoftJail/DataProcessor/Deserializer.cs
@@ -0,0 +1,41 @@
+﻿namespace SoftJail.DataProcessor
+{
+    using System.ComponentModel.DataAnnotations;
+
+    using Data;
+
+    public class Deserializer
+    {
+        private const string ErrorMessage = "Invalid Data";
+
+        private const string SuccessfullyImportedDepartment = "Imported {0} with {1} cells";
+
+        private const string SuccessfullyImportedPrisoner = "Imported {0} {1} years old";
+
+        private const string SuccessfullyImportedOfficer = "Imported {0} ({1} prisoners)";
+
+        public static string ImportDepartmentsCells(SoftJailDbContext context, string jsonString)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static string ImportPrisonersMails(SoftJailDbContext context, string jsonString)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static string ImportOfficersPrisoners(SoftJailDbContext context, string xmlString)
+        {
+            throw new NotImplementedException();
+        }
+
+        private static bool IsValid(object obj)
+        {
+            var validationContext = new System.ComponentModel.DataAnnotations.ValidationContext(obj);
+            var validationResult = new List<ValidationResult>();
+
+            bool isValid = Validator.TryValidateObject(obj, validationContext, validationResult, true);
+            return isValid;
+        }
+    }
+}
