e2338c03c7a5944102715700a1e8d0daa315672b
Alexander Valchev <106471266+AlexanderRVulchev@users.noreply.github.com>
2023-08-23 20:41:24 +0300

    Add files via upload

---
+++ Exams/07. Retake Exam - 14 August 2020/03. Data Export/SoftJail/Data/Models/Officer.cs
@@ -0,0 +1,35 @@
+﻿using SoftJail.Data.Models.Enums;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace SoftJail.Data.Models
+{
+    public class Officer
+    {
+        public Officer()
+        {
+            OfficerPrisoners = new HashSet<OfficerPrisoner>();
+        }
+
+        [Key]
+        public int Id { get; set; }
+        
+        [Required]
+        public string FullName { get; set; }
+        
+        [Required]
+        public decimal Salary { get; set; }
+        
+        [Required]
+        public Position Position { get; set; }
+        
+        [Required]
+        public Weapon Weapon { get; set; }
+        
+        [Required, ForeignKey(nameof(Department))]
+        public int DepartmentId { get; set; }
+        public Department Department { get; set; }
+        
+        public ICollection<OfficerPrisoner> OfficerPrisoners { get; set; }
+    }
+}
