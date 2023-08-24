038573bf510a93162d6d6cd16435c79c9e82508c
Alexander Valchev <106471266+AlexanderRVulchev@users.noreply.github.com>
2023-08-24 22:10:33 +0300

    Add files via upload

---
+++ Exams/07. Retake Exam - 14 August 2020/03. Data Export/SoftJail/DataProcessor/ExportDto/ExportPrisonerDto.cs
@@ -0,0 +1,27 @@
+﻿using System.Xml.Serialization;
+
+namespace SoftJail.DataProcessor.ExportDto
+{
+    [XmlType("Prisoner")]
+    public class ExportPrisonerDto
+    {
+        [XmlElement("Id")]
+        public int Id { get; set; }
+        
+        [XmlElement("Name")]
+        public string Name { get; set; }
+        
+        [XmlElement("IncarcerationDate")]
+        public string IncarcerationDate { get; set; }
+        
+        [XmlArray("EncryptedMessages")]
+        public ExportMessageDto[] EncryptedMessages { get; set; }        
+    }
+
+    [XmlType("Message")]
+    public class ExportMessageDto
+    {
+        [XmlElement("Description")]
+        public string Description { get; set; }
+    }
+}
