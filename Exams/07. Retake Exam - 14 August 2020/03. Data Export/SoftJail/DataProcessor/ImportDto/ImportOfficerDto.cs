5253b799bb5d0eea00a75303947064df684564b5
Alexander Valchev <106471266+AlexanderRVulchev@users.noreply.github.com>
2023-08-24 22:10:47 +0300

    Add files via upload

---
+++ Exams/07. Retake Exam - 14 August 2020/03. Data Export/SoftJail/DataProcessor/ImportDto/ImportOfficerDto.cs
@@ -0,0 +1,41 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Xml.Serialization;
+
+namespace SoftJail.DataProcessor.ImportDto
+{
+    [XmlType("Officer")]
+    public class ImportOfficerDto
+    {        
+        [Required]
+        [MinLength(3)]
+        [MaxLength(30)]
+        [XmlElement("Name")]
+        public string FullName { get; set; }
+        
+        [Required]
+        [XmlElement("Money")]
+        public decimal Salary { get; set; }
+        
+        [Required]
+        [XmlElement("Position")]
+        public string Position { get; set; }
+        
+        [Required]
+        [XmlElement("Weapon")]
+        public string Weapon { get; set; }
+        
+        [Required]
+        [XmlElement("DepartmentId")]
+        public int DepartmentId { get; set; }
+
+        [XmlArray("Prisoners")]
+        public ImportOfficerPrisonerDto[] Prisoners { get; set; }
+    }
+
+    [XmlType("Prisoner")]
+    public class ImportOfficerPrisonerDto
+    {
+        [XmlAttribute("id")]
+        public int PrisonerId { get; set; }        
+    }
+}
